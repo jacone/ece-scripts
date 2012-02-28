710045a92fc1cef30815fd5088a3ed07e6234a7f
Torstein Krause Johansen <tkj@vizrt.com>
2012-02-28 15:45:49 +0800

    - moved RMI hub code to its own module

---
+++ usr/share/escenic/ece-scripts/ece-install.d/rmi-hub.sh
@@ -0,0 +1,31 @@
+# ece-install module for installing the RMI hub
+
+function install_rmi_hub()
+{
+  make_dir $escenic_conf_dir/rmi-hub
+  
+  run cp -r $escenic_root_dir/engine/contrib/rmi-hub/config/* \
+    $escenic_conf_dir/rmi-hub/
+
+  hub_host=$HOSTNAME
+  file=$common_nursery_dir
+  file=$file/neo/io/managers/HubConnectionManager.properties
+
+  make_dir $(basename $file)
+  set_conf_file_value hub \
+    "rmi://${hub_host}:1099/hub/Hub" \
+    $file
+
+  cat > $common_nursery_dir/io/api/EventManager.properties <<EOF
+clientConfiguration=/neo/io/services/HubConnection
+pingTime=10000
+EOF
+
+  add_next_step "Restart all your instances to make the hub see them."
+
+  print_and_log "Starting the RMI-hub on $HOSTNAME ..."
+  ece_command="ece -t rmi-hub restart"
+  su - $ece_user -c "$ece_command" 1>>$log 2>>$log
+}
+
+
