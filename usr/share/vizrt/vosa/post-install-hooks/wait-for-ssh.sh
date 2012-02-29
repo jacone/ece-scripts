a23d7b115efa7ac49b1abeed9b875c65342fff65
Erik Mogensen <mogsie@vizrt.com>
2012-02-29 23:30:27 +0100

    Increased wait-for-ssh timeout to 90 seconds.  Also made wait-for-ssh wait for any cloud-init apt-get calls to complete, to avoid race conditions when installing on fast hardware, or when apt-get update is comparatively slow, or apt-get is busy installing lots of packages. Maybe a better way to handle this, by checking some cloud-init status.

--- usr/share/vizrt/vosa/post-install-hooks/wait-for-ssh.sh
+++ usr/share/vizrt/vosa/post-install-hooks/wait-for-ssh.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-for a in $(seq 1 20) ; do
+for a in $(seq 1 90) ; do
   ssh -q -F $2/ssh.conf root@guest id > /dev/null
   if [ $? == 0 ] ; then
+    pid=1
+    # Wait for existing apt-gets to complete.
+    while [ ! -z "$pid" ] ;
+    do
+      pid=$(ssh -F $2/ssh.conf root@guest pidof apt-get)
+      sleep 1;
+    done
     exit 0
   fi
   sleep 1;
