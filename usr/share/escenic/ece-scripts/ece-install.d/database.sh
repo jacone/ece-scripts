820032904c2e292f23dbc55f3a1cdb73810263cc
Torstein Krause Johansen <tkj@vizrt.com>
2012-02-29 14:03:51 +0800

    - new feature: new setting fai_analysis_db_install=1 for installing   the EAE DB. This is just to add consistency with the optional   fai_analysis_db_* keys. You can still use the regular fai_db_*   parameters for installing the EAE DB, this is, as   a7f864a09bf1cf3263d5b19081fafd56b04902d3 explains, only necessary to   use when you're installing multiple profiles from the same   ece-install.conf - or if you really like the idea that all EAE   parameters have the same fai_analysis_* prefix, including the db   options.
    - moved all database related code to database.sh

--- usr/share/escenic/ece-scripts/ece-install.d/database.sh
+++ usr/share/escenic/ece-scripts/ece-install.d/database.sh
@@ -1,9 +1,17 @@
+# ece-install module for installing the database.
+
 percona_rpm_release_version=0.0-1
 percona_rpm_release_package_name=percona-release-${percona_rpm_release_version}
 percona_rpm_release_url=http://www.percona.com/downloads/percona-release/$percona_rpm_release_package_name.x86_64.rpm
 if [[ $(uname -m) != "x86_64" ]]; then
   percona_rpm_release_url=http://www.percona.com/downloads/percona-release/$percona_rpm_release_package_name.i386.rpm
 fi
+
+default_db_port=3306
+default_db_host=localhost
+default_db_user=ece5user
+default_db_password=ece5password
+default_db_schema=ece5db
 
 ## $1: optional parameter, binaries_only. If passed, $1=binaries_only,
 ##     the ECE DB schema is not set up. 
@@ -87,7 +95,7 @@ function install_database_server()
 
   if [ $on_redhat_or_derivative -eq 1 ]; then
     run chkconfig --level 35 mysql on
-    run /etc/init.d/mysql start
+    run /etc/init.d/mysql restart
   fi
 
   assert_pre_requisite mysql
@@ -97,6 +105,110 @@ function install_database_server()
     download_escenic_components
     set_up_engine_and_plugins
     set_up_ecedb
+  fi
+}
+
+function set_up_ecedb()
+{
+  print_and_log "Setting up the ECE database schema ..."
+
+  make_dir $escenic_root_dir/engine/plugins
+  run cd $escenic_root_dir/engine/plugins
+  
+  find ../../ -maxdepth 1 -type d | \
+    grep -v assemblytool | \
+    while read directory; do
+    if [ $directory = "../../" ]; then
+      continue
+    fi
+    
+          # nuisance to get the community engine, but not the engine
+    if [ $(echo $directory | grep engine | wc -l) -gt 0 ]; then
+      if [ $(echo $directory | grep community | wc -l) -lt 1 ]; then
+        continue
+      fi
+    fi
+
+    if [ ! -h $(basename $directory) ]; then
+      run ln -s $directory
+    fi
+  done
+
+  # the user may override standard DB settings in ece-install.conf
+  set_db_settings_from_fai_conf
+  set_db_defaults_if_not_set
+
+  # the methods in drop-and-create-ecedb needs ece_home to be set
+  ece_home=${escenic_root_dir}/engine
+  pre_install_new_ecedb
+  create_ecedb
+  
+  cd ~/
+  run rm -rf $escenic_root_dir/engine/plugins
+
+  add_next_step "DB is now set up on ${db_host}:${db_port}"
+}
+
+function set_db_settings_from_fai_conf()
+{
+  # Note: the port isn't fully supported. The user must himself
+  # update the mysql configuration to run it on a non standard port.
+
+  # if either the profile=analysis or the profile=db &&
+  # fai_analysis_db_install is set, we try first to use the
+  # fai_analysis_db_* variables.
+  if [[ ( $install_profile_number -eq $PROFILE_DB_SERVER &&
+          $(get_boolean_conf_value fai_analysis_db_install) -eq 1 ) ||
+        $install_profile_number -eq $PROFILE_ANALYSIS_SERVER ]]; then
+
+    # order of precedence:
+    # 1) fai_analysis_db_*
+    # 2) fai_db_*
+    # 3) default_db_*
+    db_port=${fai_analysis_db_port-${fai_db_port-${default_db_port}}}
+    db_host=${fai_analysis_db_host-${fai_db_host-${default_db_host}}}
+    db_user=${fai_analysis_db_user-${fai_db_user-${default_db_user}}}
+    db_password=${fai_analysis_db_password-${fai_db_password-${default_db_password}}}
+    db_schema=${fai_analysis_db_schema-${fai_db_schema-${default_db_schema}}}
+  else
+    db_port=${fai_db_port-${default_db_port}}
+    db_host=${fai_db_host-${default_db_host}}
+    db_user=${fai_db_user-${default_db_user}}
+    db_password=${fai_db_password-${default_db_password}}
+    db_schema=${fai_db_schema-${default_db_schema}}
+  fi
+  
+  if [ -n "${fai_db_drop_old_db_first}" ]; then
+    drop_db_first=${fai_db_drop_old_db_first}
+    if [ $fai_db_drop_old_db_first -eq 1 ]; then
+      print_and_log "$(yellow WARNING): I hope you know what you're doing!"
+      print_and_log "$(yellow WARNING): fai_db_drop_old_db_first is 1 (true)"
+    fi
+  fi
+}
+
+# Method used both from interactive mode to set any missing values
+# (defaults)
+function set_db_defaults_if_not_set()
+{
+  if [ -z "$db_host" ]; then
+    db_host=${default_db_host}
+  fi
+  
+  if [ -z "$db_port" ]; then
+    db_port=${default_db_port}
+  fi
+  
+  if [ -z "$db_user" ]; then
+    db_user=${default_db_user}
+  fi
+  
+  if [ -z "$db_password" ]; then
+    db_password=${default_db_password}
+  fi
+  
+  if [ -z "$db_schema" ]; then
+    db_schema=${default_db_schema}
   fi
 }
 
