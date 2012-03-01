71fb798687bfbff9f6bb1c775e22555803456287
Torstein Krause Johansen <tkj@vizrt.com>
2012-03-01 15:22:20 +0800

    - mode the setting of publication specific Nursery configuration to   "ece deploy". This can be turned off with   do_not_add_memcached_support=1 in ece[-<instance>].conf.
      It's pretty harmless as the regular LRU cache works if the memcached
      libraries or daemons are not present.
    
    - fixed the class declaration of the memcached Nursery component.
    
    - this means that fully automatic installation and set up of memcached
      is working.
    
      TODO: test this thoroughly :-)

--- usr/share/escenic/ece-scripts/ece-install.d/memory-cache.sh
+++ usr/share/escenic/ece-scripts/ece-install.d/memory-cache.sh
@@ -2,27 +2,6 @@
 
 memcached_java_lib_url=http://img.whalin.com/memcached/jdk5/log4j/java_memcached-release_2.0.1.tar.gz
 
-## $1: root directory of the publication
-function memcached_create_publication_nursery_component() {
-  if [ ! -d "$1" ]; then
-    log $1 "doesn't exist"
-    return
-  fi
-
-  print_and_log "Adding memcached wrapper to PresenationArticle in $1 ..."
-  local dir=$1/WEB-INF/localconfig/neo/xredsys/presentation/cache
-  make_dir $dir
-  local file=$dir/PresentationArticleCache.properties
-  
-  if [[ -e $file && $(grep "\$class" $file | wc -l) -gt 0 ]]; then
-    sed -i "s#\$class=.*#\$class=neo.util.cache.Memcached#g" $file
-  else
-    echo "\$class=neo.util.cache.Memcached" >> $file
-  fi
-  
-  exit_on_error "sed on $file"
-}
-
 function install_memory_cache()
 {
   print "Installing a distributed memory cache on $HOSTNAME ..."
@@ -46,29 +25,11 @@ function install_memory_cache()
   run rm -rf $tmp_dir
   
   memcached_set_up_common_nursery
+
+  # ece deploy will set up the necessary in-publication Nursery
+  # configuration, if needed.
   
-  print_and_log "Configuring all publications for using memcached ..."
-  for el in $(ls $escenic_root_dir/assemblytool/publications/*.properties \
-    2>/dev/null); do
-    local publication=$(basename $el .properties)
-    dir=$tomcat_base/webapps/$publication
-    make_dir $dir
-    memcached_create_publication_nursery_component $dir
-  done
-  
-  # fixing the deployed publications on host
-  for el in $(
-    find $tomcat_base/webapps/ \
-      -mindepth 1 \
-      -maxdepth 1 \
-      -type d | \
-      egrep -v "solr|webservice|escenic|escenic-admin|indexer-webservice" | \
-      egrep -v "indexer-webapp|studio"
-  ); do
-    memcached_create_publication_nursery_component $el
-  done
-  
-  # TODO inform the user that he/she might want to do tihs in the
+  # TODO inform the user that he/she might want to do this in the
   # publication tree as well.
 }
 
@@ -76,7 +37,7 @@ function memcached_set_up_common_nursery() {
   local dir=$common_nursery_dir/com/danga
   make_dir $dir
   cat > $dir/SockIOPool.properties <<EOF
-$class=com.danga.MemCached.SockIOPool
+\$class=com.danga.MemCached.SockIOPool
 # fill in memcached servers here.
 servers=${fai_memcached_node_list-localhost:11211}
 
