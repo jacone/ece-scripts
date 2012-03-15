47df743d13cf47358e464604b7e9ae879b43132c
Erik Mogensen <mogsie@vizrt.com>
2012-03-15 10:36:30 +0100

    Made it possible to run several ece-installs in succession

--- usr/share/vizrt/vosa/post-install-hooks/install-engine.sh
+++ usr/share/vizrt/vosa/post-install-hooks/install-engine.sh
@@ -14,7 +14,14 @@
 # The VM is assumed to be booted and ready for SSH using the passwordless
 # key referenced by the $2/ssh.conf file, as the ubuntu or root users.
 
-# ensure we have an ece-install image.
+# ensure we have some ece-install*.conf file(s)
+ece_install_conf_files=$(ls $1 | grep "^ece-install.*\\.conf$")
+if [ -z "$ece_install_conf_files" ] ; then
+  echo "No ece-install.conf files present in $1"
+  exit 1;
+fi
+
+# ensure we have an ece-install image, download one.
 if [ ! -r $2/ece-install ] ; then
   if [ -r $1/ece-install ] ; then
     cp $1/ece-install $2
@@ -22,12 +29,13 @@ if [ ! -r $2/ece-install ] ; then
     wget "https://github.com/skybert/ece-scripts/tarball/master" -O $2/ece-install.tar.gz
   fi
 fi
-if [ ! -r $1/ece-install.conf ] ; then
-  echo "No ece-install.conf file present in $1"
-  exit 1;
-fi
 
-scp -F $2/ssh.conf $2/ece-install.tar.gz $1/ece-install.conf root@guest:
+scp -F $2/ssh.conf $2/ece-install.tar.gz $1/ece-install*.conf root@guest:
 
 ssh -F $2/ssh.conf root@guest tar xfz ece-install.tar.gz
-ssh -F $2/ssh.conf root@guest bash *-ece-scripts-*/usr/sbin/ece-install
+for conf in $ece_install_conf_files ; do
+  echo "Performing ece-install with -f $conf"
+  ssh -F $2/ssh.conf root@guest bash *-ece-scripts-*/usr/sbin/ece-install -f "$conf" || exit $? 
+  echo "---------8<-------------------------"
+done
+
