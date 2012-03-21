4ed04cd439b7ad608c6c8e641de6b4930577e23f
Torstein Krause Johansen <tkj@vizrt.com>
2012-03-21 13:20:32 +0800

    - added (moved code from ece-overview/system-info)

---
+++ usr/share/escenic/ece-scripts/system-info.d/11-list-ece-overview.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/env bash
+
+# by tkj@vizrt.com
+
+function create_ece_overview() {
+  local data="$(ece -q -i $instance info)"$'\n'
+  data="$data $(ece -q -i $instance versions | cut -d'*' -f2-)"
+  
+  echo "$data" | while read line; do
+    if [[ $line == "|->"* ]]; then
+      print_list_item $(wrap_in_anchor_if_applicable ${line:3})
+    elif [ $(echo $line | cut -d':' -f2- | wc -c) -gt 1 ]; then
+      print_list_item $(wrap_in_anchor_if_applicable $line)
+    else
+      print_un_ordered_list_end
+      print_h4_header $(echo $line | cut -d: -f1)
+      print_un_ordered_list_start
+    fi
+  done
+  
+  print_un_ordered_list_end
+}
+
+function get_overview_of_all_instances() {
+  local instance_list=$(get_instance_list)
+
+  if [ -z "$instance_list" ]; then
+    return
+  fi
+
+  print_h2_header "Overview of all the ECE instances on $HOSTNAME"
+  for el in $(get_instance_list); do
+    print_h3_header "Overview of instance $el"
+    print_un_ordered_list_start
+    create_ece_overview
+  done
+}
+
+get_overview_of_all_instances
+
