6d37ba03f5c7a7dea598f9043c93e3814be73cf2
Torstein Krause Johansen <tkj@vizrt.com>
2012-03-22 17:39:40 +0800

    - fix in system-info::db-info, will now no longer generate any output   if no MySQL DB is present

--- usr/share/escenic/ece-scripts/system-info.d/44-db-info.sh
+++ usr/share/escenic/ece-scripts/system-info.d/44-db-info.sh
@@ -2,11 +2,8 @@
 
 # by tkj@vizrt.com
 
-if [ -x /usr/sbin/mysqld ]; then
+if [ "$(which mysqld)x" != "x" ]; then
   print_h2_header "DB information"
-  print_pre_text "$(/usr/sbin/mysqld -V)"
+  print_pre_text "$(mysqld -V)"
   print_section_end
-else
-  print_pre_text "$(mysql -V 2>/dev/null)"
-  print_pre_text "$(mysql5 -V 2>/dev/null)"
 fi
