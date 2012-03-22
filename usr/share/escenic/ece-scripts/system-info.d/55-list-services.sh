764ddfa8f6282ff4d92119ec7fa5f22443d3a468
Torstein Krause Johansen <tkj@vizrt.com>
2012-03-22 16:05:41 +0800

    - for some reason, a lot of services are only listed under tcp6   instead of tcp. Mhmm. Adding them nevertheless.

--- usr/share/escenic/ece-scripts/system-info.d/55-list-services.sh
+++ usr/share/escenic/ece-scripts/system-info.d/55-list-services.sh
@@ -1,6 +1,6 @@
 
 print_h2_header "Overview of all server services on $HOSTNAME"
-print_pre_text "$(netstat -nlp 2>/dev/null | egrep -v "tcp6|ACC" | grep LISTEN)"
+print_pre_text "$(netstat -nlp 2>/dev/null | egrep -v "ACC" | grep LISTEN)"
 
 if [ $(whoami) != "root" ]; then
   print_p_text "Run $(basename $0) as  root to get more details in this listing"
