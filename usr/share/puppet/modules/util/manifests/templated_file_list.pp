2ccf8fba731a7b1fe55b3a4bc54664cf86a01fa3
Thomas Velthoven <tvelthoven@vizrt.com>
2012-04-23 06:43:36 -0700

    .

--- usr/share/puppet/modules/util/manifests/templated_file_list.pp
+++ usr/share/puppet/modules/util/manifests/templated_file_list.pp
@@ -1,11 +1,11 @@
-define util::templated_files($util_module_directory, $template_module_name, $template_directory){
+define util::templated_file_list($util_module_directory, $template_module_name, $template_directory){
 
-	$template_subdirectories = split(generate("${util_module_directory}bin/list-templates-subdirectories.sh",$template_directory) , "\n")
-	file { $template_subdirectories:
+	$template_subdirectory_list = split(generate("${util_module_directory}bin/list-templates-subdirectories.sh",$template_directory) , "\n")
+	file { $template_subdirectory_list:
 	    ensure => directory,
 	}
-	$erb_filepaths = split(generate("${util_module_directory}bin/list-templates-erb-files.sh", $template_directory), "\n")
-	util::templated_file{$erb_filepaths: 
+	$erb_filepath_list = split(generate("${util_module_directory}bin/list-templates-erb-files.sh", $template_directory), "\n")
+	util::templated_file{$erb_filepath_list: 
 		module_name => $template_module_name,
 	}
 
