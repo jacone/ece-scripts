04edd58846b3b552aed1364eac638abf563cd190
Thomas Velthoven <tvelthoven@vizrt.com>
2012-04-23 08:00:08 -0700

    added documentation

--- usr/share/puppet/modules/util/manifests/templated_file_list.pp
+++ usr/share/puppet/modules/util/manifests/templated_file_list.pp
@@ -1,4 +1,48 @@
-define util::templated_file_list($util_module_directory, $template_module_name, $template_directory){
+# = Defined type: util::templated_file_list
+#
+# This type declares a directory of templates to be deployed to a node
+#
+# == Parameters:
+#
+#	$title::								  The name of the declaration: It should start with a slash and point to the subdirectory in the 
+#														{module}/templates directory that needs to be deployed.
+#
+# $util_module_directory::  This parameter must point to the directory where 
+#														https://github.com/skybert/ece-scripts/tree/master/usr/share/puppet/modules/util
+#                   				is deployed on your puppet master
+#														default: "/etc/puppet/modules/util/"
+#
+# $template_module_name::  The module that has the templates in its templates folder.
+#
+# $module_template_directory::  The directory where the source module has its template root: 
+#																for instance: "/home/thomas/ece-scripts/usr/share/puppet/modules/escenic/templates/"
+#
+# == Actions:
+#   ensures that all files like {file}.{ext}.erb are run through the erb engine and deployed to the node as {file}.{ext}
+#
+# == Requires:
+#  all directories must have a slash at the end
+#
+# == Sample Usage:
+# 
+#			util::templated_file_list{'/etc/escenic/engine/':
+#				template_module_name => "escenic",
+#				module_template_directory => "/etc/puppet/modules/escenic/templates/",
+#				util_module_directory => "/etc/puppet/modules/util/",
+#			}
+#
+#			or better:
+#
+#			util::templated_file_list{'/etc/escenic/engine/':
+#				template_module_name => "escenic",
+#				module_template_directory => "/etc/puppet/modules/escenic/templates/",
+#			}
+#
+define util::templated_file_list($util_module_directory = "/etc/puppet/modules/util/", $template_module_name, $module_template_directory){
+
+	$template_directory = "${module_template_directory}${title}"
+	#notice($template_directory)
+	#notify{$template_directory:}
 
 	$template_subdirectory_list = split(generate("${util_module_directory}bin/list-templates-subdirectories.sh",$template_directory) , "\n")
 	file { $template_subdirectory_list:
