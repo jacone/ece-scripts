5209df3e64950a713fe53c9d8ef431e654a57ac9
Thomas Velthoven <tvelthoven@vizrt.com>
2012-04-23 08:10:14 -0700

    .

--- usr/share/puppet/modules/util/manifests/templated_file.pp
+++ usr/share/puppet/modules/util/manifests/templated_file.pp
@@ -1,6 +1,8 @@
 define util::templated_file($file_path = $title, $module_name) {
+	notice($file_path)
+	notice("${module_name}${title}.erb")
     file { $file_path:
         ensure => file,
-        content => template("${module_name}${file_path}.erb"),
+        content => template("${module_name}${title}.erb"),
     }
 }
