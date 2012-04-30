1751a784e2b7ec6490c9ac01a04a1c913f54607f
Torstein Krause Johansen <tkj@vizrt.com>
2012-04-30 11:20:54 +0200

    - only list java information if java is found in path.

--- usr/share/escenic/ece-scripts/system-info.d/44-java-info.sh
+++ usr/share/escenic/ece-scripts/system-info.d/44-java-info.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-# by tkj@vizrt.com
-
-print_h2_header "Java information"
-print_pre_text $(java -version 2>&1)
-print_section_end
+if [ $(which java 2>/dev/null | wc -l) -gt 0 ]; then
+  print_h2_header "Java information"
+  print_pre_text $(java -version 2>&1)
+  print_section_end
+fi
 
