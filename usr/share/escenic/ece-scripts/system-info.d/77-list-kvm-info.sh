5a206408148583b05c2927dde1a04ccb5ce1b205
Torstein Krause Johansen <tkj@vizrt.com>
2012-05-02 10:06:43 +0200

    - added vosa status to the system-info report (just in front of the   KVM guests)

--- usr/share/escenic/ece-scripts/system-info.d/77-list-kvm-info.sh
+++ usr/share/escenic/ece-scripts/system-info.d/77-list-kvm-info.sh
@@ -1,8 +1,17 @@
 #! /usr/bin/env bash
 
+if [ $(which vosa | wc -l) -gt 0 ]; then
+  print_h2_header "VOSA status"
+  print_un_ordered_list_start
+  vosa status | while read line; do
+    print_list_item "$line"
+  done
+  print_un_ordered_list_end
+fi
+
 if [ $(ps -u root | grep kvm | grep -v kvm-irqfd-clean | wc -l) -gt 0 ]; then
   i=1
-  print_h2_header "KVM guests on $HOSTNAME"
+  print_h2_header "KVM guests running on $HOSTNAME"
   ps auxww | grep kvm | grep .img | sed "s/^root.*kvm/kvm/" | while read line; do
     print_h3_header "KVM image #${i} on $HOSTNAME"
     print_pre_text "$line"
