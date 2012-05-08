2befc7efafe68937df460cd5868a4f14f66a3982
Erik Mogensen <mogsie@vizrt.com>
2012-05-08 10:59:31 +0200

    Removed puppet from initial boot.  Restarting cron for time zone to take effect. Fixed bug in tee.

--- usr/share/vizrt/vosa/post-install-hooks/cron-puppet-agent.sh
+++ usr/share/vizrt/vosa/post-install-hooks/cron-puppet-agent.sh
@@ -5,14 +5,17 @@ if [ $pseudorandom_minute -lt 0 ] ; then
   pseudorandom_minute=$(( - $pseudorandom_minute ))
 fi
 
+ssh -F $2/ssh.conf root@guest update-rc.d -f puppet remove
+
 echo "Installing cron job"
-ssh -F $2/ssh.conf root@guest tee /etc/cron.d/vosa-puppet-agent || exit 2 <<EOF
+ssh -F $2/ssh.conf root@guest tee /etc/cron.d/vosa-puppet-agent <<EOF
 # Installed by vosa on $(date --iso)
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 $pseudorandom_minute * * * * root puppet agent --no-daemonize --onetime
 EOF
-sleep 1;
 
 
+# Restarting cron for new time zone to take effect.
+ssh -F $2/ssh.conf root@guest service cron restart
