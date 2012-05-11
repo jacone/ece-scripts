2ad0eb95c0783b6f885ec3da17595bbf10579874
Erik Mogensen <mogsie@vizrt.com>
2012-05-11 21:02:11 +0200

    Made option to remove "temporary" information like the time of the day and the amount of free memory.
    This makes it possible to diff two successive runs of system-info to see how a system has changed.
    For system-info.d scripts, a new variable "temporaries" is set to 1 (the default) to show temporary
    information.  If set to 0, temporary information should be avoided in the output.
    
    Temporary information is defined as information which changes as a side effect of normal system
    operation, like the fill size of buffers, available memory or disk, the number or state of network
    connections and so on.

--- usr/share/escenic/ece-scripts/system-info.d/66-list-hw-info.sh
+++ usr/share/escenic/ece-scripts/system-info.d/66-list-hw-info.sh
@@ -1,14 +1,17 @@
 
-print_h2_header "Disks used on $HOSTNAME"
-print_pre_text "$(df -hT)"
-print_section_end
+if [ "$skip_temporaries" != "1" ] ; then
+  print_h2_header "Disks used on $HOSTNAME"
+  print_pre_text "$(df -hT)"
+  print_section_end
+fi
 
 print_h2_header "Memory on $HOSTNAME"
-print_pre_text "$( cat <<EOF
-Total memory: `grep MemTotal /proc/meminfo | cut -d':' -f2`
-Free memory: `grep MemFree /proc/meminfo | cut -d':' -f2`
-EOF
-)"
+local pre_text="Total memory: `grep MemTotal /proc/meminfo | cut -d':' -f2`
+"
+if [ $temporaries -eq 1 ] ; then
+  pre_text="${pre_text}Free memory: `grep MemFree /proc/meminfo | cut -d':' -f2`"
+fi
+print_pre_text "${pre_text}"
 print_section_end
 
 print_h2_header "CPU(s) on $HOSTNAME"
@@ -22,3 +25,4 @@ print_list_item "Model: \
     uniq)"
 print_un_ordered_list_end
 print_section_end
+
