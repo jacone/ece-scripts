cf57811892c6f80cc9b71888b6dd7b076d50a591
Erik Mogensen <mogsie@vizrt.com>
2012-05-12 00:20:55 +0200

    system-info: List users as preformatted text

--- usr/share/escenic/ece-scripts/system-info.d/55-list-users.sh
+++ usr/share/escenic/ece-scripts/system-info.d/55-list-users.sh
@@ -1,15 +1,9 @@
 #! /usr/bin/env bash
 
 print_h2_header "Logged in users"
-print_un_ordered_list_start
-who -H | while read line; do
-  print_list_item "$line"
-done
-print_un_ordered_list_end
+print_pre_text "$(who -H)"
 
 print_h2_header "Last Boot"
-who -b | while read line; do
-  print_pre_text "$line"
-done
+print_pre_text "$(who -b)"
 
 
