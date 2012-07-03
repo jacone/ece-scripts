1d352145a71a4e129c1406e7dfe97428e151a107
Torstein Krause Johansen <tkj@vizrt.com>
2012-07-03 13:15:47 +0200

    - added system-info module for listing IPs of a host:
      Overview of IPs on myhost:
    
        Interfaces and their IPs:
         |
          eth0      Link encap:Ethernet  HWaddr f2:3c:91:df:9e:ed
                    inet addr:192.168.193.120  Bcast:192.168.193.255  Mask:255.255.255.0
    
          tun0      Link encap:Ethernet  HWaddr 9a:e9:f4:e5:38:d7
                    inet addr:9.120.24.54  Bcast:9.120.24.254  Mask:255.0.0.0
    
        myhost's IP when accessing the world:
         |
          192.168.193.120
    
       And yes, the IPs in this example is just gibberish ;-)

---
+++ usr/share/escenic/ece-scripts/system-info.d/55-list-ips.sh
@@ -0,0 +1,10 @@
+
+print_h2_header "Overview of IPs on $HOSTNAME"
+
+print_h3_header "Interfaces and their IPs"
+print_pre_text "$(/sbin/ifconfig | grep -v inet6 | egrep -v "127.0" | grep inet -B 1 | sed 's/--//g')"
+
+print_h3_header "${HOSTNAME}'s IP when accessing the world"
+print_pre_text "$(curl --silent --connect-timeout 3 ifconfig.me 2>/dev/null)"
+
+print_section_end
