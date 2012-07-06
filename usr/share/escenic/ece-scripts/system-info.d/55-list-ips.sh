3c88fdd8bf911d08a69d64bba46661bdfbfbc8ba
Torstein Krause Johansen <torstein@cochise.vizrt.internal>
2012-07-06 12:59:39 +0200

    - increased the connection timeout from 3 to 5 when using curl to get   the public IP of the host

--- usr/share/escenic/ece-scripts/system-info.d/55-list-ips.sh
+++ usr/share/escenic/ece-scripts/system-info.d/55-list-ips.sh
@@ -5,6 +5,6 @@ print_h3_header "Interfaces and their IPs"
 print_pre_text "$(/sbin/ifconfig | grep -v inet6 | egrep -v "127.0" | grep inet -B 1 | sed 's/--//g')"
 
 print_h3_header "${HOSTNAME}'s IP when accessing the world"
-print_pre_text "$(curl --silent --connect-timeout 3 ifconfig.me 2>/dev/null)"
+print_pre_text "$(curl --silent --connect-timeout 5 ifconfig.me 2>/dev/null)"
 
 print_section_end
