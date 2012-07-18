a4ab8bf39edb6bbe24db4994ff1e1f9afea22550
Ola Schön (oschoen@vizrt.com) <ola@builder.(none)>
2012-07-18 11:54:21 +0200

    builder.sh - fixed another error in placement of patches directory

--- usr/share/escenic/build-scripts/builder.sh
+++ usr/share/escenic/build-scripts/builder.sh
@@ -448,7 +448,7 @@ function add_artifact
           if [ ! -d "$root_dir/engine/$filename" ]; then
             run mv $f $root_dir/engine/.
             # workaround for assemblytool writing into the engine directory
-            run mkdir $root_dir/engine/$f/patches
+            run mkdir $root_dir/engine/$filename/patches
           else
             print_and_log "$root_dir/engine/$filename already exists and will be ignored!"
           fi 
