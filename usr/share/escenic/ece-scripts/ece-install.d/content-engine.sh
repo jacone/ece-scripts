565663973df444270f0f0f749e3e63cfa249a287
Torstein Krause Johansen <tkj@vizrt.com>
2012-07-24 13:42:54 +0200

    - added support for EAR and conf archive when using the   profile=all. New configuration options:
      fai_all_ear
      fai_all_conf_archive

--- usr/share/escenic/ece-scripts/ece-install.d/content-engine.sh
+++ usr/share/escenic/ece-scripts/ece-install.d/content-engine.sh
@@ -576,6 +576,9 @@ function set_archive_files_depending_on_profile()
   elif [ $install_profile_number -eq $PROFILE_SEARCH_SERVER ]; then
     ece_instance_ear_file=$fai_search_ear
     ece_instance_conf_archive=$fai_search_conf_archive
+  elif [ $install_profile_number -eq $PROFILE_ALL_IN_ONE ]; then
+    ece_instance_ear_file=$fai_all_ear
+    ece_instance_conf_archive=$fai_all_conf_archive
   fi
 }
 
