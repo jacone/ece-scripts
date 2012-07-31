9e76ccf2e2cd5f0f8dc852fdcec40eb3921365da
Torstein Krause Johansen <tkj@vizrt.com>
2012-07-31 15:24:52 +0200

    - improved the service listing in system-info. Now, it only includes   the information which is interesting in a system-info context.
      The output is also numerical sorted on port number:
    
      22     629/sshd
      80     30163/varnishd
      81     30192/nginx
      3306   901/mysqld
      5666   11717/nrpe
      5678   11856/lighttpd
      6082   30094/varnishd
      8005   25239/java
      8080   25239/java
      11211  1608/memcached

--- usr/share/escenic/ece-scripts/system-info.d/55-list-services.sh
+++ usr/share/escenic/ece-scripts/system-info.d/55-list-services.sh
@@ -1,6 +1,6 @@
 
 print_h2_header "Overview of all server services on $HOSTNAME"
-print_pre_text "$(netstat -ntlp 2>/dev/null | sed -r 's,[0-9]+/([-\.a-z0-9]*).*,\1,' )"
+print_pre_text "$(netstat -ntlp 2>/dev/null | awk '{print $4, $7}' | cut -d':' -f2 | grep ^[0-9] | sort -n | uniq | column -t)"
 
 if [ $(whoami) != "root" ]; then
   print_p_text "Run $(basename $0) as  root to get more details in this listing"
