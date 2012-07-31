64a1d3d6309616fce112d4dc08edce05c275e2ce
Torstein Krause Johansen <tkj@vizrt.com>
2012-07-31 17:07:31 +0200

    - system-info now uses a cache file for storing the ifconfig.me   results (the host's IP to the outside world). This should both make   system-info faster while allowing for more accurate operation since   the ifconfig.me call, when executed, can be allowed a higher   timeout.
      If the cache file is more than 10 minutes old, it's updated.
    
    - system-info uses common-io::get_file_age_in_seconds
    
    - ifconfig.me GET timeout set to 30 seconds

--- usr/share/escenic/ece-scripts/system-info.d/55-list-ips.sh
+++ usr/share/escenic/ece-scripts/system-info.d/55-list-ips.sh
@@ -1,3 +1,20 @@
+function print_my_ip_to_the_world() {
+  local file=$HOME/.vizrt/system-info/myip.cache
+  mkdir -p $(dirname $file)
+
+  # we only query ifconfig.me every ten minutes
+  local max_age=600
+
+  if [[ ! -e $file || $(get_file_age_in_seconds $file) -gt $max_age ]]; then
+    curl --silent \
+      --connect-timeout 30 \
+      ifconfig.me \
+      2>/dev/null \
+      > $file
+  fi
+
+  cat $file
+}
 
 print_h2_header "Overview of IPs on $HOSTNAME"
 
@@ -5,6 +22,6 @@ print_h3_header "Interfaces and their IPs"
 print_pre_text "$(/sbin/ifconfig | grep -v inet6 | egrep -v "127.0" | grep inet -B 1 | sed 's/--//g')"
 
 print_h3_header "${HOSTNAME}'s IP when accessing the world"
-print_pre_text "$(curl --silent --connect-timeout 5 ifconfig.me 2>/dev/null)"
+print_pre_text "$(print_my_ip_to_the_world)"
 
 print_section_end
