94a8120b476cf1ba8e36b7e51b5358a5892884e7
Torstein Krause Johansen <tkj@vizrt.com>
2012-08-07 11:28:49 +0200

    - added .conf support to the handbook, using the ece-install trails to   make the documentation customer specific. Given that the   create-handbook.cons contains the appropriate trails, all the HTTP   links in the generated documentation should be instantly clickable   and the SSH commands should be copy and pastable (and so on).
      Here's an example of the create-handbook.conf, where the trails are
      picked up from the various .trail files generated by ece-install:
    
      trail_analysis_host=analysis.hosting.provider.com
      trail_builder_host=builder.vizrtsaas.com
      trail_builder_user=tomato
      trail_control_host=control.hosting.provider.com
      trail_website_name=tomato
      trail_monitoring_host=mon.hosting.provider.com
      trail_editor_host=edit1.hosting.provider.com
      trail_import_host=edit2.hosting.provider.com
      trail_nfs_server_host=nfs1.hosting.provider.com
      trail_presentation_host=pres1.hosting.provider.com
      trail_presentation_host_list="$trail_presentation_host pres2.hosting.provider.com"
      trail_db_schema=tomatodb
      trail_escenic_backup_dir=/var/backups/escenic
      trail_webapp_name=tomato
    
    - added ECE architecture diagram

--- usr/share/doc/vizrt/vosa-handbook/create-handbook.sh
+++ usr/share/doc/vizrt/vosa-handbook/create-handbook.sh
@@ -3,6 +3,7 @@
 # by tkj@vizrt.com
 
 handbook_org=vosa-handbook.org
+target_dir=target
 
 $(which blockdiag >/dev/null) || {
   cat <<EOF
@@ -12,24 +13,71 @@ EOF
   exit 1
 }
 
-# generate the diagram
-for el in graphics/*.blockdiag; do
-  echo "Generating PNG of $el ..."
-  blockdiag $el
-done
+function generate_png_from_blockdiag() {
+  cp -r graphics $target_dir
+  
+  for el in $target_dir/graphics/*.blockdiag; do
+    echo "Generating PNG of $el ..."
+    blockdiag $el
+  done
+}
+
+function set_customer_specific_variables() {
+  local conf_file=$(basename $0 .sh).conf
+  if [ -r $conf_file  ]; then
+    source $conf_file
+  else
+    return
+  fi
+
+  customer_filter_map="
+    my-build-server~${trail_builder_host}
+    my-build-user~${trail_builder_user}
+    my-control-server~${trail_control_host}
+    my-db~${trail_db_schema}
+    my-editorial-server~${trail_editor_host}
+    my-import-server~${trail_import_host}
+    my-import-server~${trail_import_host}
+    my-monitoring-server~${trail_monitoring_host}
+    my-nfs-server~${trail_nfs_server_host}
+    my-presentation-server~${trail_presentation_host}
+    my-stats-server~${trail_analysis_host}
+    my-webapp~${trail_website_name}
+    my-website~${trail_website_name}
+  "
+
+  for el in $customer_filter_map; do
+    local old_ifs=$IFS
+    IFS='~'
+    read from to <<< "$el"
+    IFS=$old_ifs
+    find $target_dir -name "*.org" | while read f; do
+      sed -i "s~${from}~${to}~g" ${f}
+    done
+  done
+}
+
+function set_up_build_directory() {
+  mkdir -p $target_dir
+  cp *.org $target_dir
+}
+
+set_up_build_directory
+generate_png_from_blockdiag
+set_customer_specific_variables
 
 # use emacs to generate HTML from the ORG files
 echo "Generating new handbook HTML from ORG ..." 
-emacs --batch --visit $handbook_org \
-  --funcall org-export-as-html-batch 2>/dev/null || {
+emacs \
+  --load vizrt-branding-org-mode.el \
+  --batch --visit $target_dir/$handbook_org \
+  --funcall org-export-as-html-batch 2> /dev/null || {
   cat <<EOF
-You must have Emacs 24 + org mode from git to use this export
-function.
+Failed running the org export through Emacs.
 EOF
-  exit 1
 }
 
-echo "$(basename $handbook_org .org).html is now ready"
+echo "$target_dir/$(basename $handbook_org .org).html is now ready"
 
 
 
