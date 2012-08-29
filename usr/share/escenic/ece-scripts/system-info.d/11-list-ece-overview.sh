2ada23d762fcf5924310298be91ddad0fb41265d
Torstein Krause Johansen <tkj@vizrt.com>
2012-08-29 10:21:06 +0200

    - improved the readability of the log analysis, now printing more as   normal text so that it can be wrapped nicely, and only monotyping   commands and listings that really need it.

--- usr/share/escenic/ece-scripts/system-info.d/11-list-ece-overview.sh
+++ usr/share/escenic/ece-scripts/system-info.d/11-list-ece-overview.sh
@@ -1,9 +1,6 @@
 #! /usr/bin/env bash
 
 # by tkj@vizrt.com
-
-
-
 function get_instance_type() {
   local type="engine"
 
@@ -60,6 +57,7 @@ function create_ece_overview() {
 
   if [ ${temporaries-1} -eq 1 ]; then
     list_error_overview_for_instance $1
+    list_import_overview_for_instance $1
   fi
 }
 
@@ -95,6 +93,81 @@ function get_overview_of_all_instances() {
   done
 
   print_section_end
+}
+
+function check_picture_file() {
+  local dir=$(dirname "$1")
+  local file_name=$(basename "$1")
+
+  if [ ! -d $dir ]; then
+    print_p_text "Directory doesn't exist: $dir" \
+      "It's referenced via the image: $1"
+    return 
+  fi
+  
+  local alternative=$(find $dir -maxdepth 1 | grep -i "$file_name")
+
+  # first check if the image is size=0
+  if [ $(find $dir -name $file_name -maxdepth 1 -type f -size 0 | wc -l) -gt 0 ]; then
+    print_pre_text "The file $1 is empty (size = 0 bytes)"
+    return
+  fi
+
+  if [[ -n "$alternative" && $(echo $alternative | wc -l) -gt 0 ]]; then
+    if [[ "$1" == "$alternative" ]]; then
+      print_p_text "The XML refered to ${1}" \
+        "which exists. This means either that the file wasn't there before when" \
+        "the import failed or that the file itself is unusable."
+      return
+    fi
+
+    print_p_text "The file $1" \
+      "was missing from your import data set It was referred to from one of" \
+      "your XML files, perhaps you meant this one?" \
+      $alternative \
+      "Suggested command to fix this:"
+    print_pre_text "mv \"$alternative\" \"$1\""
+  fi
+}
+
+## $1 is thefile
+function list_sax_errors() {
+  local file=/var/log/escenic/${HOSTNAME}-${1}-messages
+  local result="$(grep ^'Caused by: org.xml.sax.SAXParseException:' $file | sort | uniq)"
+  
+  if [ -z $result ]; then
+    return
+  fi
+  
+  print_p_text "There are illegal elements, attributes" \
+    "and/or contents in the XML:"
+  print_pre_text "$result"
+}  
+
+function list_import_overview_for_instance() {
+  if [ ${generate_import_job_overview-0} -eq 0 ]; then
+    return
+  fi
+  
+  local file=/var/log/escenic/${HOSTNAME}-${1}-messages
+  
+  if [ ! -e $file ]; then
+    return
+  fi
+  
+  print_h4_header "Overview of ${1}'s import jobs"
+  
+  grep "Invalid image file" $file | \
+    grep ^java.io.IOException | \
+    cut -d' ' -f5 | \
+    sed 's/\.$//g' | \
+    sort | \
+    uniq | \
+    while read picture_path; do
+    check_picture_file "$picture_path"
+  done
+  
+  list_sax_errors "$1"
 }
 
 get_overview_of_all_instances
