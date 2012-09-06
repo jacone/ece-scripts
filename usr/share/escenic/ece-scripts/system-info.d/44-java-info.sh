b90b0dcf54042a1d8b02533c42e18d56b0e0d856
Torstein Krause Johansen <tkj@vizrt.com>
2012-09-06 13:26:27 +0200

    - now uses the java defined in /etc/escenic/ece.conf, if it exists,   instead of the java in PATH.
      If ece.conf doesn't exist or isn't readable, the java, if available,
      in PATH is used.

--- usr/share/escenic/ece-scripts/system-info.d/44-java-info.sh
+++ usr/share/escenic/ece-scripts/system-info.d/44-java-info.sh
@@ -1,8 +1,19 @@
 #! /usr/bin/env bash
 
-if [ $(which java 2>/dev/null | wc -l) -gt 0 ]; then
+java_bin=$(which java)
+file=/etc/escenic/ece.conf
+
+if [ -r $file ]; then
+  # don't like sourcing ece.conf, so getting it from grep instead.
+  the_java_home=$(grep ^java_home $file | cut -d'=' -f2)
+  if [ -n "$the_java_home" && -d "$the_java_home" ]; then
+    java_bin=$the_java_home/bin/java
+  fi
+fi
+
+if [ $(ls $java_bin 2>/dev/null | wc -l) -gt 0 ]; then
   print_h2_header "Java information"
-  print_pre_text $(java -version 2>&1)
+  print_pre_text $(${java_bin} -version 2>&1)
   print_section_end
 fi
 
