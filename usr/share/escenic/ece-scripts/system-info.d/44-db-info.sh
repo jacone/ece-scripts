3df7cff9d61e12540ae5f7a628b57babe100968d
Torstein Krause Johansen <tkj@vizrt.com>
2012-09-28 11:05:32 +0200

    - fixed error on systems with no mysqld, when running system-info from   the command line

--- usr/share/escenic/ece-scripts/system-info.d/44-db-info.sh
+++ usr/share/escenic/ece-scripts/system-info.d/44-db-info.sh
@@ -2,7 +2,7 @@
 
 # by tkj@vizrt.com
 
-if [ "$(which mysqld)x" != "x" ]; then
+if [ "$(which mysqld 2>/dev/null)x" != "x" ]; then
   print_h2_header "DB information"
   print_pre_text "$(mysqld -V)"
   print_section_end
