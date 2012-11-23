c32d96a127ede1a7fbc08853ad4f2a081f0290be
Torstein Krause Johansen <tkj@vizrt.com>
2012-11-23 15:51:16 +0100

    - added common_bashing_exit_hook: To make your script call this   whenever it does a controlled exit, either by running through the   script, call this hook
    - added common_bashing_user_cancelled_hook: Use this in your script to
      have it exit whenever the user hits the user pressing Ctrl+c or by
      someone sending a regular kill <PID> signal to it.

--- usr/share/escenic/ece-scripts/common-bashing.sh
+++ usr/share/escenic/ece-scripts/common-bashing.sh
@@ -436,8 +436,7 @@ function create_lock_or_fail() {
 ## $1 :: the lock file
 function remove_lock() {
   if [ ! -e $lock_file ]; then
-    print "Something is seriously wrong, the lock file ($lock_file) isn't there!"
-    exit 1
+    return
   fi
 
   fail_safe_run rm $lock_file
@@ -453,3 +452,27 @@ function remove_pid() {
     fail_safe_run rm $pid_file
   fi
 }
+
+## To make your script call this whenever it does a controlled exit,
+## either by running through the script, call this hook.
+##
+## Put this line at the start of your script:
+##
+## trap common_bashing_exit_hook EXIT
+function common_bashing_exit_hook() {
+  remove_pid
+  remove_lock
+  kill $$
+}
+
+## Put this in your script to have it exit whenever the user hits the
+## user pressing Ctrl+c or by someone sending a regular kill <PID>
+## signal to it.
+## 
+## Usage:
+## trap common_bashing_user_cancelled_hook SIGHUP SIGINT
+function common_bashing_user_cancelled_hook() {
+  print "User cancelled $(basename $0), cleaning up after me ..."
+  common_bashing_exit_hook
+}
+
