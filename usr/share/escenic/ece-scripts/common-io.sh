644c5ae91613812405a99825ecd5996caf3bfb18
Torstein Krause Johansen <tkj@vizrt.com>
2012-11-30 14:38:16 +0100

    - common-io::download_uri_target_to_dir now includes the HTTP response   headers. This is very useful, for instance when the download fails   because of authorisation. This information will be present in the   logs.

--- usr/share/escenic/ece-scripts/common-io.sh
+++ usr/share/escenic/ece-scripts/common-io.sh
@@ -103,7 +103,12 @@ function download_uri_target_to_dir() {
   
   if [[ $uri == "http://"* || $uri == "https://"* ]]; then
     run cd $target_dir
-    run wget $wget_opts $wget_auth $uri -O $(basename $uri)
+    run wget \
+      $wget_opts \
+      $wget_auth \
+      --server-response \
+      --output-document $(basename $uri) \
+      $uri
     file=$target_dir/$(basename $uri)
   elif [[ $uri == "file://"* ]]; then
       # length of file:// is 7
