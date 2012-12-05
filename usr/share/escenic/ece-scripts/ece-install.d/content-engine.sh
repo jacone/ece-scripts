24d104ed686345de81b5843185efe77089880c45
Torstein Krause Johansen <tkj@vizrt.com>
2012-12-05 14:17:26 +0100

    - fixed the deploy white list for the all in one profile (old bug)
    - fixed the deploy white list in general where it's now taken from the
      fai_<profile>_deploy_white_list variables depending on the
      installation profile.

--- usr/share/escenic/ece-scripts/ece-install.d/content-engine.sh
+++ usr/share/escenic/ece-scripts/ece-install.d/content-engine.sh
@@ -1,12 +1,12 @@
 # ece-install module Content Engine specific code.
 
 function get_deploy_white_list() {
-  
+
   local deploy_white_list=""
   local presentation_deploy_white_list="escenic-admin $(get_publication_short_name_list)"
   local editor_deploy_white_list="escenic-admin escenic studio indexer-webservice webservice"
   local search_deploy_white_list="solr indexer-webapp"
-  
+
   # user's deploy white list, if present, takes precedence.
   if [ $fai_enabled -eq 1 ]; then
     if [ $install_profile_number -eq $PROFILE_PRESENTATION_SERVER ]; then
@@ -15,6 +15,13 @@ function get_deploy_white_list() {
       deploy_white_list=${fai_editor_deploy_white_list-$editor_deploy_white_list}
     elif [ $install_profile_number -eq $PROFILE_SEARCH_SERVER ]; then
       deploy_white_list=${fai_search_deploy_white_list-$search_deploy_white_list}
+    elif [ $install_profile_number -eq $PROFILE_ALL_IN_ONE ]; then
+      local default_all_deploy_white_list="
+        ${presentation_deploy_white_list}
+        ${editor_deploy_white_list}
+        ${search_deploy_white_list}
+      "
+      deploy_white_list=${fai_all_deploy_white_list-$default_all_deploy_white_list}
     else
       deploy_white_list=${fai_publication_deploy_white_list-""}
     fi
@@ -24,23 +31,42 @@ function get_deploy_white_list() {
 }
 
 function get_publication_short_name_list() {
+  local deploy_white_list=""
+
+  if [ $install_profile_number -eq $PROFILE_PRESENTATION_SERVER ]; then
+    deploy_white_list=${fai_presentation_deploy_white_list}
+  elif [ $install_profile_number -eq $PROFILE_EDITORIAL_SERVER ]; then
+    deploy_white_list=${fai_editor_deploy_white_list}
+  elif [ $install_profile_number -eq $PROFILE_SEARCH_SERVER ]; then
+    deploy_white_list=${fai_search_deploy_white_list}
+  elif [ $install_profile_number -eq $PROFILE_ALL_IN_ONE ]; then
+    deploy_white_list=${fai_all_deploy_white_list}
+  fi
+
+  if [[ -z "$deploy_white_list" && \
+    -n "${fai_publication_domain_mapping_list}" ]]; then
+    for el in $fai_publication_domain_mapping_list; do
+      log el=$el
+
+      old_ifs=$IFS
+      IFS='#' read publication domain aliases <<< "$el"
+      log publication=$publication domain=$domain aliases=$aliases
+      IFS=',' read publication_name publication_war <<< "$publication"
+      log publication_name=$publication_name publication_war=$publication_war
+      IFS=$old_ifs
+      if [ -z "${publication_war}" ]; then
+        publication_war=${publication_name}.war
+      fi
+      deploy_white_list="${deploy_white_list} $(basename $publication_war .war)"
+    done
+  fi
+
   local short_name_list=""
-  
-  local publication_def_dir=${escenic_root_dir}/assemblytool/publications
 
-  if [ ! -d $publication_def_dir ]; then
-    echo ${short_name_list}
-    return
-  fi
-
-  if [ $(ls ${publication_def_dir} | grep .properties$ | wc -l) -eq 0 ]; then
-    echo ${short_name_list}
-    return
-  fi
-
-  for el in $(find ${publication_def_dir} -maxdepth 1 -name "*.properties"); do
-    local short_name=$(basename $el .properties)
-    short_name_list="${short_name_list} ${short_name}"
+  for el in $deploy_white_list; do
+    if [ $(is_webapp_a_publication $el) -eq 1 ]; then
+      short_name_list="${short_name_list} $el"
+    fi
   done
 
   echo ${short_name_list}
@@ -49,13 +75,13 @@ function get_publication_short_name_list() {
 ## $1=<default instance name>
 function install_ece_instance() {
   install_ece_third_party_packages
-  
+
   ask_for_instance_name $1
   set_up_engine_directories
   set_up_ece_scripts
 
   set_archive_files_depending_on_profile
-  
+
   download_escenic_components
   check_for_required_downloads
   set_up_engine_and_plugins
@@ -78,10 +104,9 @@ function install_ece_instance() {
     set_up_basic_nursery_configuration
     set_up_instance_specific_nursery_configuration
   fi
-  
+
   # We set a WAR white list for all profiles except all in one
-  if [ $install_profile_number -ne $PROFILE_ALL_IN_ONE -a \
-    $install_profile_number -ne $PROFILE_ANALYSIS_SERVER ]; then
+  if [ $install_profile_number -ne $PROFILE_ANALYSIS_SERVER ]; then
     local file=$escenic_conf_dir/ece-${instance_name}.conf
     print_and_log "Creating deployment white list in $file ..."
     set_conf_file_value \
@@ -95,7 +120,7 @@ function install_ece_instance() {
     install_memory_cache
     assemble_deploy_and_restart_type
   fi
-  
+
   update_type_instances_to_start_up
   set_conf_file_value ece_unix_user $ece_user /etc/default/ece
   set_conf_file_value ece_unix_group $ece_group /etc/default/ece
@@ -117,7 +142,7 @@ function set_up_engine_and_plugins() {
   if [ $ece_software_setup_completed -eq 1 ]; then
     return
   fi
-  
+
   print_and_log "Setting up the Escenic Content Engine & its plugins ..."
   make_dir $escenic_root_dir
   run cd $escenic_root_dir/
@@ -132,13 +157,13 @@ function set_up_engine_and_plugins() {
       engine_file=$(basename $el)
     fi
   done
-  
+
   if [ -n "$engine_dir" -a ! -d "${engine_dir}" ]; then
     run unzip -q -u -o $download_dir/${engine_file}
     if [ -h engine ]; then
       run rm engine
     fi
-    
+
     run ln -s ${engine_dir} engine
   else
     debug "${engine_dir} is already there, skipping to next step."
@@ -160,10 +185,10 @@ function set_up_engine_and_plugins() {
 
 function set_up_assembly_tool() {
   log "Setting up the Assembly Tool ..."
-  
+
   make_dir $escenic_root_dir/assemblytool/
   cd $escenic_root_dir/assemblytool/
-  
+
   if [ -e $download_dir/assemblytool*zip ]; then
     run unzip -q -u -o $download_dir/assemblytool*zip
   fi
@@ -176,7 +201,7 @@ function set_up_assembly_tool() {
   run cp -r layers/host layers/instance
   run cp -r layers/host layers/vosa
   run cp -r layers/host layers/server
-  
+
   cat > layers/environment/Files.properties <<EOF
 \$class=neo.nursery.FileSystemDepot
 fileSystemRoot = $escenic_conf_dir/engine/environment/\${com.escenic.environment "unknown"}/
@@ -217,7 +242,7 @@ layer.05 = /layers/family/Layer
 # Customer :: environment layer (e.g. testing, staging, production)
 layer.06 = /layers/environment/Layer
 
-# Customer :: host specific layer 
+# Customer :: host specific layer
 layer.07 = /layers/host/Layer
 
 # Customer :: instance specific layer
@@ -234,7 +259,7 @@ EOF
   do
     sed -i "s#/etc/escenic#${escenic_conf_dir}#g" $el
   done
-  
+
   # set up which plugins to use
   run cd $escenic_root_dir/assemblytool/
   make_dir plugins
@@ -246,7 +271,7 @@ EOF
       $(echo $directory | grep widget-framework | wc -l) -gt 0 ]; then
       continue
     fi
-    
+
     # nuisance to get the community engine and analysis engine, but
     # not the engine
     if [ $(echo $directory | \
@@ -266,7 +291,7 @@ EOF
     assemble.properties \
     1>>$log 2>>$log
   exit_on_error "sed on assemble.properties"
-  
+
   sed -i "s~\#\# plugins\ =\ /path/to/plugins~plugins=${escenic_root_dir}/assemblytool/plugins~g" \
     assemble.properties \
     1>>$log 2>>$log
@@ -277,14 +302,14 @@ EOF
   # set up user publication definitions
   if [ -n "${fai_publication_war_uri_list}" ]; then
     run cd $escenic_root_dir/assemblytool/publications
-    
+
     wget_auth=$wget_builder_auth
     for el in ${fai_publication_war_uri_list}; do
       download_uri_target_to_dir \
         $el \
         $escenic_root_dir/assemblytool/publications
       local download_file=$escenic_root_dir/assemblytool/publications/$(basename $el)
-      
+
       if [ ! -e $(basename $el) ]; then
         print_and_log "Failed to get user publication $el." \
           "I will skip it and continue to the next one."
@@ -322,7 +347,7 @@ function set_up_basic_nursery_configuration() {
     # the conf archive typically resides on the build server, hence we
     # set its authentication credentials here.
     wget_auth=$wget_builder_auth
-    
+
     local a_tmp_dir=$(mktemp -d)
     download_uri_target_to_dir \
       $ece_instance_conf_archive \
@@ -331,7 +356,7 @@ function set_up_basic_nursery_configuration() {
 
     run cd $a_tmp_dir
     extract_archive $file
-    
+
     if [ -d ${a_tmp_dir}/engine/security ]; then
       run cp -r $a_tmp_dir/engine/security/ $common_nursery_dir/
     else
@@ -339,7 +364,7 @@ function set_up_basic_nursery_configuration() {
       print "I'll use standard JAAS (engine/security) instead."
       run cp -r $escenic_root_dir/engine/security/ $common_nursery_dir/
     fi
-    
+
     run cp -r engine/siteconfig/config-skeleton/* $common_nursery_dir/
   else
     print_and_log "I'm using Nursery & JAAS configuration (skeleton) " \
@@ -352,7 +377,7 @@ function set_up_basic_nursery_configuration() {
   if [ -n "${a_tmp_dir}" ]; then
     run rm -rf ${a_tmp_dir}
   fi
-  
+
   public_host_name=$HOSTNAME:${appserver_port}
   if [ $fai_enabled -eq 1 ]; then
     if [ -n "$fai_public_host_name" ]; then
@@ -368,7 +393,7 @@ function set_up_basic_nursery_configuration() {
   if [ -n "$user_host_name" ]; then
     public_host_name=$user_host_name
   fi
-  
+
   cat > $common_nursery_dir/ServerConfig.properties <<EOF
 databaseProductName=MySQL
 filePublicationRoot=$escenic_data_dir/engine/
@@ -433,9 +458,9 @@ function set_up_publication_nursery_conf() {
       IFS=','
       read publication_name publication_war <<< "$publication"
       IFS=$old_ifs
-      
+
       local file=$escenic_conf_dir/engine/environment/${fai_publication_environment-production}/neo/publications/Pub-${publication_name}.properties
-      
+
       print_and_log "Creating Nursery component" \
         $(basename $file .properties) \
         "for publication $publication_name"
@@ -451,7 +476,7 @@ EOF
 
 function set_up_instance_specific_nursery_configuration() {
   print_and_log "Setting up instance specific Nursery configuration ..."
-  
+
   for el in $escenic_conf_dir/engine/instance/*; do
     i=$(( i + 1 ))
     if [ $(basename $el) = $instance_name ]; then
@@ -463,7 +488,7 @@ function set_up_instance_specific_nursery_configuration() {
   nursery_context=neo/io/managers/HubConnectionManager.properties
   file=$escenic_conf_dir/engine/instance/$instance_name/$nursery_context
   make_dir $(dirname $file)
-  
+
   # we don't touch it if the file already exists.
   if [ ! -e $file ]; then
     run echo "hostname=$HOSTNAME" >> $file
@@ -477,7 +502,7 @@ function install_ece_third_party_packages
 {
   run_hook install_ece_third_party_packages.preinst
   print_and_log "Installing 3rd party packages needed by $type instances"
-  
+
   if [ $on_debian_or_derivative -eq 1 ]; then
     if [ $on_ubuntu -eq 1 ]; then
       # Sun Java was removed in Ubuntu in 11.10 and later also from
@@ -485,7 +510,7 @@ function install_ece_third_party_packages
       # (2012-02-20 11:22)
       local version_needs_local_java_deb=1004
       local version=$(lsb_release -s -r | sed "s#\.##g")
-      
+
       add_apt_source \
         "deb http://archive.canonical.com/ $(lsb_release -s -c) partner"
     elif [ $on_debian -eq 1 ]; then
@@ -509,7 +534,7 @@ function install_ece_third_party_packages
 
     if [ $(has_sun_java_installed) -eq 0 ]; then
       create_java_deb_packages_and_repo
-      
+
       # Here, we install sun-java6-jdk first so that ant doesn't pull
       # down OpenJDK.
       #
@@ -520,7 +545,7 @@ function install_ece_third_party_packages
         debconf-set-selections
       install_packages_if_missing sun-java6-jdk
     fi
-    
+
     local packages="
       ant
       ant-contrib
@@ -546,7 +571,7 @@ function install_ece_third_party_packages
     # TODO no tomcat APR wrappers in official repositories
     install_sun_java_on_redhat
   fi
-  
+
   install_packages_if_missing $packages
   assert_commands_available ant java jar
 }
@@ -622,7 +647,7 @@ function assemble_deploy_and_restart_type()
   else
     print_and_log "Assembling, deploying & starting $instance_name ..."
   fi
-  
+
   su - $ece_user -c "$ece_command" 1>>$log 2>>$log
   exit_on_error "su - $ece_user -c \"$ece_command\""
 }
@@ -678,7 +703,7 @@ function set_up_search_client_nursery_conf() {
   make_dir $dir
   echo "solrServerURI=http://${search_host}:${search_port}/solr" \
     >>  $dir/SolrSearchEngine.properties
-  
+
   dir=$common_nursery_dir/com/escenic/webservice/search
   make_dir $dir
   echo "solrURI=http://${search_host}:${search_port}/solr/select" \
@@ -693,5 +718,4 @@ function set_up_search_client_nursery_conf() {
   make_dir $dir
   echo "solrURI=http://${search_host}:${search_port}/solr" \
     >> $dir/SearchEngine.properties
-}  
-
+}
