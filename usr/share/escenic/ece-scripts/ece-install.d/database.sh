jacone/ece-scripts

commit 235fe6aa319c6692d3ffe4c2a1a7118f12a82416

- made fai_analysis_db_install=1 a proper profile. - smarter use of line wrapping

usr/share/escenic/ece-scripts/ece-install.d/database.sh
@@ -33,7 +33,7 @@ function set_up_percona_repository_if_possible() {
     fi
 
     if [ $supported_code_name -eq 1 ]; then
-      print_and_log "Setting Up the Percona repository ..."
+      print_and_log "Setting Up the Percona Repository ..."
 
       if [ $(apt-key list| grep CD2EFD2A | wc -l) -lt 1 ]; then
         # this CANNOT be run in the run wrapper since it often fails,
@@ -118,7 +118,7 @@ function install_mysql_client_software() {
 ## $1: optional parameter, binaries_only. If passed, $1=binaries_only,
 ##     the ECE DB schema is not set up.
 function install_database_server() {
-  print_and_log "Installing database server on $HOSTNAME ..."
+  print_and_log "Installing a database server on $HOSTNAME ..."
 
   if [ ${fai_db_sql_only-0} -eq 0 ]; then
     install_mysql_server_software
@@ -173,7 +173,7 @@ function set_up_ecedb() {
   pre_install_new_ecedb
   create_ecedb
 
-  add_next_step "DB is now set up on ${db_host}:${db_port}"
+  add_next_step "DB ${db_schema} is now set up on ${db_host}:${db_port}"
 }
 
 function set_db_settings_from_fai_conf()
@@ -181,22 +181,18 @@ function set_db_settings_from_fai_conf()
   # Note: the port isn't fully supported. The user must himself
   # update the mysql configuration to run it on a non standard port.
 
-  # if either the profile=analysis or the profile=db &&
-  # fai_analysis_db_install is set, we try first to use the
-  # fai_analysis_db_* variables.
-  if [[ ( $install_profile_number -eq $PROFILE_DB_SERVER &&
-          $(get_boolean_conf_value fai_analysis_db_install) -eq 1 ) ||
-        $install_profile_number -eq $PROFILE_ANALYSIS_SERVER ]]; then
-
+  # if the analysis or analysis DB profile is selected, these are the
+  # only ones that care about analysis specific DB settings.
+  if [[ $install_profile_number -eq $PROFILE_ANALYSIS_DB_SERVER ||
+        $install_analysis_server -eq $PROFILE_ANALYSIS_SERVER ]]; then
     # order of precedence:
     # 1) fai_analysis_db_*
-    # 2) fai_db_*
-    # 3) default_db_*
-    db_port=${fai_analysis_db_port-${fai_db_port-${default_db_port}}}
-    db_host=${fai_analysis_db_host-${fai_db_host-${default_db_host}}}
-    db_user=${fai_analysis_db_user-${fai_db_user-${default_db_user}}}
-    db_password=${fai_analysis_db_password-${fai_db_password-${default_db_password}}}
-    db_schema=${fai_analysis_db_schema-${fai_db_schema-${default_db_schema}}}
+    # 2) default_db_*
+    db_port=${fai_analysis_db_port-${default_db_port}}
+    db_host=${fai_analysis_db_host-${default_db_host}}
+    db_user=${fai_analysis_db_user-${default_db_user}}
+    db_password=${fai_analysis_db_password-${default_db_password}}
+    db_schema=${fai_analysis_db_schema-${default_db_schema}}
   else
     db_port=${fai_db_port-${default_db_port}}
     db_host=${fai_db_host-${default_db_host}}

usr/sbin/ece-install
@@ -1070,6 +1070,13 @@ if [ $fai_enabled -eq 1 ]; then
         no_fai_profile=0
     fi
 
+
+    if [ ${fai_analysis_db_install-0} -eq 1 ]; then
+      install_profile_number=$PROFILE_ANALYSIS_DB_SERVER
+      install_database_server
+      no_fai_profile=0
+    fi
+    
     # Important that the EAE is installed after all, editor and
     # presentation in case it's running on the same host as any of
     # them. This because it configures the EAE ECE plugin, which the

usr/share/escenic/ece-scripts/common-bashing.sh
@@ -49,7 +49,9 @@ function print() {
   fi
 
   # we break the text early to have space for the ID.
-  echo $@ | fmt --width 65 | sed "s#^#$(get_id) #g"
+  local id="$(get_id) "
+  local text_width=$(( 80 - $(echo $id | wc -c) ))
+  echo $@ | fmt --width $text_width | sed "s#^#${id}#g"
 }
 
 function printne() {

usr/share/escenic/ece-scripts/ece-install.d/constants.sh
@@ -13,6 +13,7 @@ PROFILE_CREATE_PUBLICATION=9
 PROFILE_MONITORING_SERVER=10
 PROFILE_RESTORE_FROM_BACKUP=11
 PROFILE_ANALYSIS_SERVER=12
+PROFILE_ANALYSIS_DB_SERVER=12
 PROFILE_VIP_PROVIDER=13
 PROFILE_NFS_SERVER=14
 PROFILE_NFS_CLIENT=15

usr/share/escenic/ece-scripts/ece-install.d/monitoring.sh
@@ -198,8 +198,8 @@ function install_nagios_node()
       print_and_log "check_mk might not start automatically, as xinetd.d/check_mk is missing"
     fi
   else
-    print_and_log "Nagios node installation not supported on your system"
-    print_and_log "You will have to install it manually."
+    print_and_log "Nagios node installation not supported on your system," \
+      "you will have to install it manually."
     return
   fi
 
@@ -217,8 +217,8 @@ function install_nagios_node()
   run touch /var/run/nagios/nrpe.pid
   run /etc/init.d/nagios-nrpe-server restart
 
-  add_next_step "A Nagios NRPE node and check_mk has been installed"
-  add_next_step "on ${HOSTNAME}"
+  add_next_step "A Nagios NRPE node and check_mk has been installed" \
+    "on ${HOSTNAME}"
 }
 
 function install_munin_node()
@@ -250,8 +250,8 @@ function install_munin_node()
     packages="munin-node munin-plugins-extra"
     install_packages_if_missing $packages
   else
-    print_and_log "Munin node installation not supported on your system"
-    print_and_log "You will have to install it manually."
+    print_and_log "Munin node installation not supported on your system," \
+      "you will have to install it manually."
     return
   fi
 
@@ -343,14 +343,14 @@ function install_munin_gatherer()
     packages="munin"
     install_packages_if_missing $packages
   else
-    print_and_log "Munin gatherer installation not supported on your"
-    print_and_log "system :-( You will have to install it manually."
+    print_and_log "Munin gatherer installation not supported on your" \
+      "system :-( You will have to install it manually."
     return
   fi
 
   if [ $fai_enabled -eq 0 ]; then
-    print "Which nodes shall this Munin monitor gather from?"
-    print "Separate your hosts with a space, e.g.: 'editor01 db01 web01'"
+    print "Which nodes shall this Munin monitor gather from?" \
+      "Separate your hosts with a space, e.g.: 'editor01 db01 web01'"
     echo -n "Your choice> "
     read user_munin_nodes
 
@@ -394,8 +394,8 @@ EOF
     run /etc/init.d/apache2 reload
   fi
 
-  add_next_step "Munin gatherer admin interface: http://${HOSTNAME}/munin"
-  add_next_step "Make sure all nodes allows its IP to connect to them."
+  add_next_step "Munin gatherer admin interface: http://${HOSTNAME}/munin" \
+    "Make sure all nodes allows its IP to connect to them."
 }
 
 ## $1 nagios vendor
@@ -579,6 +579,6 @@ function install_system_info() {
     done
   fi
 
-  add_next_step "Always up to date system info: http://$HOSTNAME:$port/"
-  add_next_step "you can also see system-info in the shell, type: system-info"
+  add_next_step "Always up to date system info: http://$HOSTNAME:$port/" \
+    "you can also see system-info in the shell, type: system-info"
 }

commit 735dcd1571aa6a8c73ad31e7c196e72efdaaca80

- fixed a subtle bug introduced in 235fe6aa

usr/share/escenic/ece-scripts/ece-install.d/database.sh
@@ -182,7 +182,7 @@ function set_db_settings_from_fai_conf()
   # if the analysis or analysis DB profile is selected, these are the
   # only ones that care about analysis specific DB settings.
   if [[ $install_profile_number -eq $PROFILE_ANALYSIS_DB_SERVER ||
-        $install_analysis_server -eq $PROFILE_ANALYSIS_SERVER ]]; then
+        $install_profile_number -eq $PROFILE_ANALYSIS_SERVER ]]; then
     # order of precedence:
     # 1) fai_analysis_db_*
     # 2) default_db_*