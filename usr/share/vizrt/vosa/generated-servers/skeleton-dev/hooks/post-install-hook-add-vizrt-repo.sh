072f1205b7ba8659d76f514988faf7e4e54efaf5
Ola Schön (oschoen@vizrt.com) <oschoen@vizrt.com>
2013-01-22 11:21:55 +0100

    vosa - visrt repo hook will now do apt-get update

--- usr/share/vizrt/vosa/generated-servers/skeleton-dev/hooks/post-install-hook-add-vizrt-repo.sh
+++ usr/share/vizrt/vosa/generated-servers/skeleton-dev/hooks/post-install-hook-add-vizrt-repo.sh
@@ -8,4 +8,7 @@ curl -s http://apt.vizrt.com/archive.key | ssh -F $2/ssh.conf guest 'sudo apt-ke
 # add repo to sources list
 echo "deb http://apt.vizrt.com unstable main" | ssh -F $2/ssh.conf guest 'sudo tee > /dev/null /etc/apt/sources.list.d/vizrt.list'
 
+# update package lists
+ssh -F $2/ssh.conf guest 'sudo apt-get update'
+
 echo "--- END : post install hook - add vizrt repo ---"
