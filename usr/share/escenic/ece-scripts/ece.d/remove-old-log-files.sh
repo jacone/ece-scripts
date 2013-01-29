70ea8d99273e51437872ce1ea38002aedbd1328a
Torstein Krause Johansen <tkj@vizrt.com>
2013-01-29 13:12:43 +0100

    - removed shebang as this is a module and not a standalone command.

--- usr/share/escenic/ece-scripts/ece.d/remove-old-log-files.sh
+++ usr/share/escenic/ece-scripts/ece.d/remove-old-log-files.sh
@@ -1,5 +1,3 @@
-#! /usr/bin/env bash
-
 # removes log files older than <numbber> days
 old_log_file_max_age_in_days=5
 
@@ -16,7 +14,7 @@ function get_log_file_dir_list() {
 
 function remove_old_logs_if_exist() {
   debug "Looking for old log files in $1 ..."
-  
+
   local old_log_files="$(
     find -L $1 -mtime +${old_log_file_max_age_in_days} -type f
   )"
@@ -25,7 +23,8 @@ function remove_old_logs_if_exist() {
     return
   fi
 
-  print_and_log "Deleting" $(echo "$old_log_files" | wc -l) \
+  print_and_log "Deleting" \
+    $(echo "$old_log_files" | wc -l) \
     "log files in $1" \
     "older than ${old_log_file_max_age_in_days} days"
   run rm $old_log_files
