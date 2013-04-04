f2c5f3e154413a1689e1273218317dbd24c237c6
Ola Schön <oschoen@vizrt.com>
2013-04-04 14:17:04 +0200

    ece-dev-image - libxml2-utils is a requirement for ece-deploy and will now be installed by default

--- usr/share/vizrt/vosa/generated-servers/skeleton-dev/hooks/post-install-hook-install-ece-install.sh
+++ usr/share/vizrt/vosa/generated-servers/skeleton-dev/hooks/post-install-hook-install-ece-install.sh
@@ -3,6 +3,7 @@
 echo "--- START : post install hook - install ece install ---"
 
 # install escenic scripts
+ssh -F $2/ssh.conf guest 'sudo apt-get -y install libxml2-utils'
 ssh -F $2/ssh.conf guest 'sudo apt-get update && sudo apt-get -y install escenic-content-engine-installer'
 ssh -F $2/ssh.conf guest 'sudo apt-get update && sudo apt-get -y install escenic-content-engine-scripts'
 
