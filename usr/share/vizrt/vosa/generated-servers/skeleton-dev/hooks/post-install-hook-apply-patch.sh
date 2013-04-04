7b34c50795ce4a95f3d3e2f78bf8e655e0809d3c
Ola Schön <oschoen@vizrt.com>
2013-04-04 14:18:34 +0200

    ece-dev-image - removing jar files on solr instance to save space is no longer viable

--- usr/share/vizrt/vosa/generated-servers/skeleton-dev/hooks/post-install-hook-apply-patch.sh
+++ usr/share/vizrt/vosa/generated-servers/skeleton-dev/hooks/post-install-hook-apply-patch.sh
@@ -13,21 +13,6 @@ ssh -F $2/ssh.conf root@guest  \
 ssh -F $2/ssh.conf root@guest  \
    cp -R /opt/tomcat/webapps/manager /opt/tomcat-search1/webapps/.
 
-# cleanup all jar files not needed to run solr and analysis engine
-ssh -F $2/ssh.conf root@guest  \
-   'find /opt/tomcat-search1/escenic/lib/ -type f ! \( \
--name 'common-nursery-*' \
--o -name 'commons-codec-*' \
--o -name 'commons-httpclient-*' \
--o -name 'commons-io-*' \
--o -name 'commons-lang-*' \
--o -name 'commons-logging-*' \
--o -name 'common-util-*' \
--o -name 'log4j-*' \
--o -name 'twelvemonkeys-core-*' \
--o -name 'xom-*' \
-\) -exec rm -rf {} \;'
-
 # change ownership just to be sure
 ssh -F $2/ssh.conf root@guest  \
    chown -R escenic: /opt/tomcat-engine1
