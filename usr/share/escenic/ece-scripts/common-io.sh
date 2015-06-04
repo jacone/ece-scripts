jacone/ece-scripts

commit 7aba83f8147c03948ce1ff1f2704d7e3aa9bc971

VOSA-133 Fixed failed download of java_memcached-release_2.5.3.jar from github by using curl instead of wget

usr/share/escenic/ece-scripts/common-io.sh
@@ -160,6 +160,34 @@ function download_uri_target_to_dir() {
   fi
 }
 
+### curl_download_uri_target_to_dir
+## Returns the local, downloaded file with absolute path.
+##
+## $1 :: http://server/file or
+##       https://file. The $1 should only be a http/https url
+## $2 :: the target dir
+## $3 :: the target filename. If left empty, the bassename of $1 is used
+function curl_download_uri_target_to_dir() {
+  local uri=$1
+  local target_dir=$2
+  local target_file=$3
+  make_dir $target_dir
+
+  if [[ $uri == "http://"* || $uri == "https://"* ]]; then
+
+    if [ -z $target_file ] ; then
+      target_file=$(basename $uri)
+    fi
+    
+    log "Downloading" $uri "to" $target_dir "..."
+    run cd $target_dir
+    run curl \
+      -L \
+      --o "$target_file" \
+      $uri
+  fi
+}
+
 ### verify_that_archive_is_ok
 ## Will verify that the archive passed to the function is ok. If it's
 ## not, the function will terminate the calling script, exiting in

usr/share/escenic/ece-scripts/ece-install.d/memory-cache.sh
@@ -14,7 +14,7 @@ function install_memory_cache()
   assert_commands_available memcached
 
   print_and_log "Downloading the memcached java library ..."
-  download_uri_target_to_dir $memcached_java_lib_url $download_dir
+  curl_download_uri_target_to_dir $memcached_java_lib_url $download_dir
 
   make_dir ${escenic_root_dir}/assemblytool/lib
   run cp $(basename $memcached_java_lib_url) ${escenic_root_dir}/assemblytool/lib

commit 3b6d7b368d2e25ca66258c022a36fb6caea15825

Fixed bug in curl_download_uri_target_to_dir
- curl has no --o (it has -o, see man curl), regression bug introduced
  in 7aba83f8

- replaced curl short options with long options as they're easier to read.

- removed trailing white space

usr/share/escenic/ece-scripts/common-io.sh
@@ -124,7 +124,7 @@ function download_uri_target_to_dir() {
     if [ -z $target_file ] ; then
       target_file=$(basename $uri)
     fi
-    
+
     log "Downloading" $uri "to" $target_dir "..."
     run cd $target_dir
     run wget \
@@ -144,8 +144,8 @@ function download_uri_target_to_dir() {
     run s3cmd get $uri $target_file
 
   else
-    
-    local file=$uri    
+
+    local file=$uri
     if [[ $uri == "file://"* ]] ; then
       # length of file:// is 7
       file=${file:7}
@@ -178,12 +178,12 @@ function curl_download_uri_target_to_dir() {
     if [ -z $target_file ] ; then
       target_file=$(basename $uri)
     fi
-    
+
     log "Downloading" $uri "to" $target_dir "..."
     run cd $target_dir
     run curl \
-      -L \
-      --o "$target_file" \
+      --location \
+      --output "$target_file" \
       $uri
   fi
 }