8f0930a7f0aec002e12a6f1e8899a93cd11201eb
Torstein Krause Johansen <torstein@escenic.com>
2016-05-11 15:14:04 +0200

    Updated fall back Tomcat URL

--- usr/share/escenic/ece-scripts/common-os.sh
+++ usr/share/escenic/ece-scripts/common-os.sh
@@ -8,7 +8,7 @@
 common_bashing_is_loaded > /dev/null 2>&1 || source $(pwd)/common-bashing.sh
 
 ## Only used if the Tomcat download mirror couldn't be determined
-fallback_tomcat_url="http://apache.uib.no/tomcat/tomcat-7/v7.0.62/bin/apache-tomcat-7.0.62.tar.gz"
+fallback_tomcat_url="http://apache.uib.no/tomcat/tomcat-7/v7.0.69/bin/apache-tomcat-7.0.69.tar.gz"
 
 # Can be used like this:
 # common_io_os_loaded 2>/dev/null || source common-os.sh
