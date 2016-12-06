4d1ed58c494087812ea4b8f1640a972db2fd04d7
Sk Mohd Anwarul Islam <anwar@escenic.com>
2016-12-06 09:42:24 +0100

    Changed reference of apt.vizrt.com to apt.escenic.com

--- usr/share/vizrt/vosa/post-install-hooks/add-vizrt-apt-repo.sh
+++ usr/share/vizrt/vosa/post-install-hooks/add-vizrt-apt-repo.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
 ## Adds the Vizrt APT repository
-echo deb http://apt.vizrt.com unstable main | \
-  ssh -F $2/ssh.conf guest sudo tee /etc/apt/sources.list.d/apt-vizrt-com.list \
+echo deb http://apt.escenic.com unstable main | \
+  ssh -F $2/ssh.conf guest sudo tee /etc/apt/sources.list.d/apt.escenic.com.list \
   > /dev/null
 
-curl -s http://apt.vizrt.com/archive.key | \
+curl -s http://apt.escenic.com/archive.key | \
   ssh -F $2/ssh.conf guest sudo apt-key add - \
   > /dev/null
 
