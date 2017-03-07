jacone/ece-scripts

commit 75989b29bfbe3b6ffdb6f1554641d39d449f1846

Added support for tomcat download url override (change provided by Imran).

usr/share/escenic/ece-scripts/common-os.sh
@@ -123,12 +123,16 @@ function assert_commands_available() {
 }
 
 function get_tomcat_download_url() {
-  local url=$(
-    curl -s http://tomcat.apache.org/download-60.cgi | \
-      grep tar.gz | \
-      head -1 | \
-      cut -d'"' -f2
-  )
+  if [ -n $tomcat_download ]; then  [15:28]
+    local url=$tomcat_download
+  else
+    local url=$(
+        curl -s http://tomcat.apache.org/download-60.cgi | \
+            grep tar.gz | \
+            head -1 | \
+            cut -d'"' -f2
+    )
+  fi
 
   if [ -z $url ]; then
     url=$fallback_tomcat_url

commit 8deda4fae11fca6b6aa1816f5c882f004d899793

Fixed regression in common-os::get_tomcat_download_url()
- the regression bug made ece-install skip looking up the correct
  Tomcat download URL, instead always falling back to the
  fallback_tomcat_url. Because of this, we had to update the
  fallback_url all the time, or provide one in the ece-install.conf
  file. This is no longer needed.

- the regression was introduced in
  75989b29bfbe3b6ffdb6f1554641d39d449f1846

usr/share/escenic/ece-scripts/common-os.sh
@@ -5,7 +5,8 @@
 # by torstein@escenic.com
 
 # depends on common-bashing
-common_bashing_is_loaded > /dev/null 2>&1 || source $(pwd)/common-bashing.sh
+common_bashing_is_loaded > /dev/null 2>&1 ||
+  source "${BASH_SOURCE[0]%/*}/common-bashing.sh"
 
 ## Only used if the Tomcat download mirror couldn't be determined
 fallback_tomcat_url="http://apache.uib.no/tomcat/tomcat-7/v7.0.70/bin/apache-tomcat-7.0.70.tar.gz"
@@ -134,10 +135,11 @@ function assert_commands_available() {
 }
 
 function get_tomcat_download_url() {
-  if [ -n $tomcat_download ]; then
-    local url=$tomcat_download
+  local url=
+  if [ -n "${tomcat_download}" ]; then
+    url=$tomcat_download
   else
-    local url=$(
+    url=$(
         curl -s http://tomcat.apache.org/download-70.cgi | \
             grep tar.gz | \
             head -1 | \
@@ -145,7 +147,7 @@ function get_tomcat_download_url() {
     )
   fi
 
-  if [ -z $url ]; then
+  if [ -z "${url}" ]; then
     url=$fallback_tomcat_url
     log "Failed to get Tomcat mirror URL, will use fallback URL $url"
   fi

usr/local/src/unit-tests/common-os-test.sh
@@ -1,0 +1,26 @@
+#! /usr/bin/env bash
+## author: torstein@escenic.com
+
+test_can_get_tomcat_download_url_should_not_get_fallback() {
+  unset tomcat_download
+  local actual=
+  actual=$(get_tomcat_download_url)
+
+  assertNotEquals "Shouldn't get the fallback url" "${fallback_tomcat_url}" "${actual}"
+}
+
+## @override shunit2
+setUp() {
+  source "$(dirname "$0")/../../../share/escenic/ece-scripts/common-os.sh"
+}
+
+## @override shunit2
+tearDown() {
+  :
+}
+
+main() {
+  . "$(dirname "$0")"/shunit2/source/2.1/src/shunit2
+}
+
+main "$@"