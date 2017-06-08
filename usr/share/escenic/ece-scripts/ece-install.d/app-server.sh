jacone/ece-scripts

commit 39b19903f3060ea3ae33a9ccb00288ce09e0558a

Updated Maria JDBC driver version to 2.0.1
- the old 1.0 version had issues with truncating bytes when writing
  strings to the database (!).

- This bug could, among other tings, lead to invalid section page XML
  blobs.

usr/share/escenic/ece-scripts/ece-install.d/app-server.sh
@@ -3,7 +3,7 @@ function set_up_jdbc_library() {
   if [ -n "$jdbc_driver" -a -e "$jdbc_driver" ]; then
     make_ln $jdbc_driver
   elif [ $db_vendor = "mariadb" ]; then
-    local mariadb_jdbc_url=https://downloads.mariadb.org/f/mariadb-java-client-1.1.0/mariadb-java-client-1.1.0.jar
+    local mariadb_jdbc_url= https://downloads.mariadb.com/Connectors/java/connector-java-2.0.1/mariadb-java-client-2.0.1.jar
     print_and_log "Downloading MariaDB jdbc driver ${mariadb_jdbc_url}"
     local mariadb_jdbc_jar=${mariadb_jdbc_url##*/}
     download_uri_target_to_dir \

commit bde5cf888afa557e9e59d7999f24871e23c0e883

One, 1!, space broke the code.
- regression introduced in 39b19903f3060ea3ae33a9ccb00288ce09e0558a

usr/share/escenic/ece-scripts/ece-install.d/app-server.sh
@@ -3,7 +3,7 @@ function set_up_jdbc_library() {
   if [ -n "$jdbc_driver" -a -e "$jdbc_driver" ]; then
     make_ln $jdbc_driver
   elif [ $db_vendor = "mariadb" ]; then
-    local mariadb_jdbc_url= https://downloads.mariadb.com/Connectors/java/connector-java-2.0.1/mariadb-java-client-2.0.1.jar
+    local mariadb_jdbc_url=https://downloads.mariadb.com/Connectors/java/connector-java-2.0.1/mariadb-java-client-2.0.1.jar
     print_and_log "Downloading MariaDB jdbc driver ${mariadb_jdbc_url}"
     local mariadb_jdbc_jar=${mariadb_jdbc_url##*/}
     download_uri_target_to_dir \