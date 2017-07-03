jacone/ece-scripts

commit 9f4822ac07da361cb0a4c19ad2572a27207b25d7

With Varnish 4, there's no reason for date hack
In Varnish 3, date was incorrectly always set to "now" making it hard
to do the right thing with the Age header, setting it to 0 was our
only solution, see
https://www.varnish-cache.org/lists/pipermail/varnish-commit/2014-April/011368.html

However, this has been fixed in Varnish 4, so that the date header
from origin is kept. This means we can remove the hard coded Age: 0
response header.

usr/share/escenic/ece-scripts/ece-install.d/cache-server.sh
@@ -477,18 +477,6 @@ EOF
   # at the end of the vcl_deliver above.
 }
 
-function create_varnish_conf_hacks() {
-  local file=${varnish_conf_dir}/varnish-hacks.vcl
-  cat > $file <<EOF
-sub vcl_deliver {
-  /* It seems that Varnish changes the Date header and the age header
-   * becomes wrong because of this. For this reason, we set the Age
-   * header to 0 to be standards compliant. */
-  set resp.http.Age = "0";
-}
-EOF
-}
-
 function get_from_domain() {
   if [[ $1 == "^www." ]]; then
     echo $1 | sed 's/^www.//g'
@@ -594,7 +582,6 @@ function create_varnish_conf() {
   create_varnish_conf_robots_on_beta
   create_varnish_conf_cookie_cleaner
   create_varnish_conf_caching_policies
-  create_varnish_conf_hacks
   create_varnish_conf_redirects
   create_varnish_conf_cache_statistics
   create_varnish_conf_error_pages

commit ccbae92e6f0c4d6fab4bbe6df3a416f4f7eea85b

Regression fix: varnish-hacks.vcl are no longer needed
- regression introduced in 9f4822ac07da361cb0a4c19ad2572a27207b25d7

usr/share/escenic/ece-scripts/ece-install.d/cache-server.sh
@@ -73,7 +73,6 @@ include "compression.vcl";
 include "robots-on-beta.vcl";
 include "cookie-cleaner.vcl";
 include "caching-policies.vcl";
-include "varnish-hacks.vcl";
 include "redirects.vcl";
 include "cache-statistics.vcl";
 include "error-pages.vcl";