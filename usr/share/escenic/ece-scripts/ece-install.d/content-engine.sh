jacone/ece-scripts

commit 740b16cfcabf87a7f5d02662fecc36c51356833e

Now installs Oracle JDK on Debian & Ubuntu
- pre-seeding a machine with the JDK is no longer necessary
- from before, RedHat/CentOS machines install JDK without
  pre-seeding.
- removed + rewrote large parts of old java installation code
- no need to set java_home anymore, however, a new conf entry is needed:

environment:
  # Setting this to true means you've read & accepted
  # http://www.oracle.com/technetwork/java/javase/terms/license/index.html
  java_oracle_licence_accepted: true

usr/share/escenic/ece-scripts/ece-install.d/java.sh
@@ -1,36 +1,36 @@
-default_java_version=1.6
+default_java_version=1.8
+_java_version_with_build_number=${fai_java_version-${fai_server_java_version-8u121-b13}}
+_java_version=${_java_version_with_build_number%-*}
 sun_java_bin_url=http://download.oracle.com/otn-pub/java/jdk/6u39-b04/jdk-6u39-linux-i586.bin
 if [[ $(uname -m) == "x86_64" ]]; then
-  sun_java_bin_url=http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.rpm
+  sun_java_bin_url=http://download.oracle.com/otn-pub/java/jdk/${_java_version_with_build_number}/e9e7ea248e2c4826b92b3f075a80e441/jdk-${_java_version}-linux-x64.rpm
 fi
   
-# Install oracle java from webupd8
-# http://www.webupd8.org/2012/01/install-oracle-java-jdk-7-in-ubuntu-via.html
-function install_oracle_java(){
-  # First get the java version
+function install_oracle_java() {
+  if _java_is_sun_java_already_installed; then
+    print_and_log "Oracle Java is already installed on $HOSTNAME"
+    _java_update_java_env_from_java_bin
+    return
+  fi
+
+  if [ "${fai_java_oracle_licence_accepted-0}" -eq 0 ]; then
+    print_and_log \
+      "You must accept Oracle's licence:" \
+      "http://www.oracle.com/technetwork/java/javase/terms/license/index.html" \
+      "YAML conf: environment.java_oracle_licence_accpted = true" \
+      "Conf: fai_java_oracle_licence_accepted=1"
+    remove_pid_and_exit_in_error
+  fi
+
+  print_and_log "Downloading Oracle Java from download.oracle.com ..."
   server_java_version=${fai_server_java_version-${default_java_version}}
   debian_package_name=""
 
-  if [ $server_java_version = "1.6" ]; then
-    debian_package_name="oracle-java6-installer"
-    java_home="/usr/lib/jvm/java-6-oracle"
-  elif [ $server_java_version = "1.7" ]; then
-    debian_package_name="oracle-java7-installer"
-    java_home="/usr/lib/jvm/java-7-oracle"
-  #elif [ $server_java_version = "1.8" ]; then
-  #  debian_package_name="oracle-java8-installer"
-  #  java_home="/usr/lib/jvm/java-8-oracle"
-  else
-    # Error: Invalid java version
-    print_and_log "Unsupported Java version. Cannot proceed. Exiting ...."
-    return 0
+  if [ "${on_debian_or_derivative-0}" -eq 1 ]; then
+    _install_oracle_java_debian
+  elif [ "${on_redhat_or_derivative-0}" -eq 1 ]; then
+    _install_oracle_java_redhat
   fi
-
-  install_packages_if_missing python-software-properties software-properties-common
-  run add-apt-repository -y ppa:webupd8team/java
-  run apt-get update
-  echo "$debian_package_name shared/accepted-oracle-license-v1-1 boolean true" | debconf-set-selections
-  install_packages_if_missing $debian_package_name
 }
 
 ## Returns 0 if Oracle Java is installed, 1 if it's not (in PATH).
@@ -66,6 +66,17 @@ function _java_update_java_env_from_jdk_rpm() {
   _java_update_java_env_from_jdk_dir "${rpm_java_home}"
 }
 
+function _java_update_java_env_from_jdk_deb() {
+  local deb=$1
+  local deb_java_home=
+  deb_java_home=$(
+    dpkg -c "${deb}" |
+      sed -n 's#/bin/javac##p' |
+      sed 's#.*[.]##')
+
+  _java_update_java_env_from_jdk_dir "${deb_java_home}"
+}
+
 function _java_update_java_env_from_java_bin() {
   local java_bin=
   java_bin=$(which java)
@@ -93,21 +104,54 @@ function _java_update_java_env_from_java_bin() {
   _java_update_java_env_from_jdk_dir "${jdk_dir}"
 }
 
-function install_sun_java_on_redhat() {
-  if _java_is_sun_java_already_installed; then
-    print_and_log "Sun Java is already installed on $HOSTNAME"
-    _java_update_java_env_from_java_bin
-    return
-  fi
-  
+## $1 :: uri
+## $2 :: target file
+function _download_oracle_file() {
   print_and_log "Downloading Oracle Java from download.oracle.com ..."
-  local file_name=${download_dir}/${sun_java_bin_url##*/}
+  local uri=$1
+  local target_file=$2
   run wget \
       --no-cookies \
-      --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com ; oraclelicense=accept-securebackup-cookie" \
-      -O $file_name \
-      $wget_opts \
-      $sun_java_bin_url
+      --header 'Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com ; oraclelicense=accept-securebackup-cookie' \
+      -O "${target_file}" \
+      --inet4-only \
+      --continue \
+      "${uri}"
+}
+
+function _install_oracle_java_debian() {
+  print_and_log "Creating a DEB package from the Oracle JDK tarball ..."
+  sun_java_bin_url=${sun_java_bin_url//.rpm/.tar.gz}
+  local file_name=${download_dir}/${sun_java_bin_url##*/}
+  _download_oracle_file "${sun_java_bin_url}" "${file_name}"
+
+  # dependencies for make-jpkg (which the package currently doesn't
+  # define as such)
+  install_packages_if_missing "
+    build-essential
+    fakeroot
+    gcc
+    java-common
+    java-package
+    libgtk2.0-0
+  "
+
+  # make-jpkg doesn't heed DEBIAN_FRONTEND=noninteractive. Also, old
+  # versions (jessie) of make-jpkg insists on using fakeroot, so we
+  # use ${ece_user} for this as this user has been created in
+  # ece-install::common_pre_install
+  run su - ${ece_user} -c "make-jpkg ${file_name} <<< Y"
+  run mv $(getent passwd ${ece_user} | cut -d: -f6)/*${_java_version}*.deb \
+      "${download_dir}/."
+  run dpkg -i "${download_dir}/"*${_java_version}*.deb
+
+  _java_update_java_env_from_jdk_deb "${download_dir}/"*${_java_version}*.deb
+}
+
+function _install_oracle_java_redhat() {
+  print_and_log "Downloading & installing Oracle JDK RPM ..."
+  local file_name=${download_dir}/${sun_java_bin_url##*/}
+  _download_oracle_file "${sun_java_bin_url}" "${file_name}"
 
   if ! is_rpm_already_installed "${file_name}"; then
     run rpm -Uvh "${file_name}"
@@ -121,6 +165,4 @@ function install_sun_java_on_redhat() {
   add_next_step "By using Oracle Java, you must accept this license: " \
     "http://www.oracle.com/technetwork/java/javase/terms/license/"
 }
-
-
 

usr/local/src/unit-tests/ece-install-conf-file-reader-test.sh
@@ -22,6 +22,7 @@ environment:
   type: ${environment_type}
   java_home: ${foo_java_home}
   java_version: ${foo_java_version}
+  java_oracle_licence_accepted: true
   skip_password_checks: true
   conf_url: ${conf_url}
   apt:
@@ -43,7 +44,8 @@ EOF
 
   unset java_home
   unset fai_environment
-  unset fai_server_java_version
+  unset fai_java_version
+  unset fai_java_oracle_licence_accepted
   unset fai_maven_repositories
   unset fai_conf_url
   unset fai_package_rpm_base_url
@@ -53,9 +55,12 @@ EOF
 
   assertNotNull "Should set java_home" "${java_home}"
   assertEquals "Should set java_home" "${foo_java_home}" "${java_home}"
-  assertEquals "Should set fai_server_java_version" \
+  assertEquals "Should set fai_java_version" \
                "${foo_java_version}" \
-               "${fai_server_java_version}"
+               "${fai_java_version}"
+  assertEquals "Should set fai_java_oracle_licence_accepted" \
+               1 \
+               "${fai_java_oracle_licence_accepted}"
   assertEquals "Should set fai_environment (type)" \
                "${environment_type}" \
                "${fai_environment}"

usr/sbin/ece-install
@@ -952,31 +952,10 @@ function read_user_input() {
   done
 }
 
-function get_java_package_name() {
-  # The script will install oracle java installer package on Debian based
-  # systems and this is the path of the JAVA home with this package. If
-  # you're using a different system or have other preferences, change
-  # java_home in your ece-install.conf
-  default_java_version=1.6
-  server_java_version=${fai_server_java_version-${default_java_version}}
-  if [ $server_java_version = "1.6" ]; then
-    java_package_name="oracle-java6-installer"
-  elif [ $server_java_version = "1.7" ]; then
-    java_package_name="oracle-java7-installer"
-  #elif [ $server_java_version = "1.8" ]; then
-  #  java_package_name="oracle-java8-installer"
-  else
-    # Setting it back to default
-    java_package_name=""
-  fi
-}
-
-
 function main() {
     read_user_input "$@"
     init
     assert_correct_runtime_environment
-    get_java_package_name
 
     print_and_log "Full Automatic Install (FAI)."
     print_and_log "All user input will be read from $conf_file"

usr/share/escenic/ece-scripts/common-os.sh
@@ -217,7 +217,12 @@ function add_apt_source() {
     return
   fi
 
-  if [ "$(grep -r "${url}" /etc/apt/sources.list* | wc -l)" -lt 1 ]; then
+  # Remove HTTP scheme and user/pass when checking if the source
+  # already exists.
+  local url_fragment=${url##*://}
+  url_fragment=${url_fragment##*@}
+
+  if [ "$(grep -r "${url_fragment}" /etc/apt/sources.list* | wc -l)" -lt 1 ]; then
     echo "# added by $(basename $0) @ $(date)" >> $escenic_sources
     echo "$@" >> $escenic_sources
   fi

usr/share/escenic/ece-scripts/ece-install.d/conf-file-reader.sh
@@ -278,10 +278,18 @@ _parse_yaml_conf_file_environment() {
     export java_home=${configured_java_home}
   fi
 
+  local configured_java_oracle_licence_accepted=
+  configured_java_oracle_licence_accepted=$(
+    _jq "${yaml_file}" .environment.java_oracle_licence_accepted)
+  if [[ "${configured_java_oracle_licence_accepted}" == "yes" ||
+          "${configured_java_oracle_licence_accepted}" == "true" ]]; then
+    export fai_java_oracle_licence_accepted=1
+  fi
+
   local configured_java_version=
   configured_java_version=$(_jq "${yaml_file}" .environment.java_version)
   if [[ -n "${configured_java_version}" ]]; then
-    export fai_server_java_version=${configured_java_version}
+    export fai_java_version=${configured_java_version}
   fi
 
   local configured_environment=

commit 9680d377374b26d638e435d87904f24ac6d67035

Now installs Oracle Java on RedHat systems (again)
- fixes regression introduced in
  740b16cfcabf87a7f5d02662fecc36c51356833e

usr/share/escenic/ece-scripts/ece-install.d/content-engine.sh
@@ -773,7 +773,7 @@ function install_ece_third_party_packages
       xmlstarlet
     "
 
-    install_sun_java_on_redhat
+    install_oracle_java
   fi
 
   install_packages_if_missing $packages

usr/share/escenic/ece-scripts/ece-install.d/java.sh
@@ -1,15 +1,29 @@
 # -*- mode: sh; sh-shell: bash; -*-
 
+oracle_jdk_download_url=http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
+
 function _java_get_oracle_tarball_url() {
   if [ -n "${fai_java_download_url-""}" ]; then
     echo "${fai_java_download_url}"
     return
   fi
 
-  local url=http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
-  curl -s  "${url}" |
+  curl -s  "${oracle_jdk_download_url}" |
     grep "$(uname -s) x64" |
     grep .tar.gz |
+    grep -v demos |
+    sed -n -r  's#.*filepath":"(.*)", "MD5".*#\1#p'
+}
+
+function _java_get_oracle_rpm_url() {
+  if [ -n "${fai_java_download_url-""}" ]; then
+    echo "${fai_java_download_url}"
+    return
+  fi
+
+  curl -s  "${oracle_jdk_download_url}" |
+    grep "$(uname -s) x64" |
+    grep .rpm |
     grep -v demos |
     sed -n -r  's#.*filepath":"(.*)", "MD5".*#\1#p'
 }
@@ -159,8 +173,12 @@ function _install_oracle_java_debian() {
 
 function _install_oracle_java_redhat() {
   print_and_log "Downloading & installing Oracle JDK RPM ..."
-  local file_name=${download_dir}/${sun_java_bin_url##*/}
-  _download_oracle_file "${sun_java_bin_url}" "${file_name}"
+
+  local oracle_java_rpm_url=
+  oracle_java_rpm_url=$(_java_get_oracle_rpm_url)
+
+  local file_name=${download_dir}/${oracle_java_rpm_url##*/}
+  _download_oracle_file "${oracle_java_rpm_url}" "${file_name}"
 
   if ! is_rpm_already_installed "${file_name}"; then
     run rpm -Uvh "${file_name}"
@@ -168,7 +186,8 @@ function _install_oracle_java_redhat() {
 
   _java_update_java_env_from_jdk_rpm "${file_name}"
 
-  local version=$(java -version 2>&1 | grep version | cut -d'"' -f2)
+  local version=
+  version=$(java -version 2>&1 | grep version | cut -d'"' -f2)
   print_and_log "Oracle Java $version is now installed"
 
   add_next_step "By using Oracle Java, you must accept this license: " \